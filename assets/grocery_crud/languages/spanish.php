588b8ec9bdd3329422b3cc48999760c3ffcf001b
John Skoumbourdis <scoumbourdisj@gmail.com>
2011-12-27 19:28:01 +0200

    Adding Bulgarian to the multilingual functionality - Bulgarian translated by fantas

---
+++ assets/grocery_crud/languages/spanish.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by fantas */
+	$lang['list_add']				= 'Agregar';
+	$lang['list_actions']			= 'Acciones';
+	$lang['list_page']				= 'Pagina';  
+	$lang['list_paging_of']			= 'de';
+	$lang['list_displaying']		= 'Mostrando {start} a {end} de {results} registros';
+	$lang['list_filtered_from']		= '(filtered de {total_results} total entradas)';
+	$lang['list_show_entries']		= 'Mostrar {paging} entradas';
+	$lang['list_no_items']			= 'No hay registros para mostrar';
+	$lang['list_zero_entries']		= 'Mostrando 0 a 0 de 0 registros';
+	$lang['list_search']			= 'Buscar';    
+	$lang['list_search_all']		= 'Buscar todos';
+	$lang['list_clear_filtering']   = 'Limpiar filtrados';
+	$lang['list_delete']			= 'Borrar';
+	$lang['list_edit']				= 'Editar';  
+	$lang['list_paging_first']		= 'Primero';
+	$lang['list_paging_previous']	= 'Anterior';
+	$lang['list_paging_next']		= 'Siguiente';
+	$lang['list_paging_last']		= 'Ultimo';
+	$lang['list_loading']			= 'Cargando...';          
+
+	$lang['form_edit']				= 'Editar';
+	$lang['form_back_to_list']		= 'Regresar a la lista';
+	$lang['form_update_changes']	= 'Actualizar cambios';
+	$lang['form_cancel']			= 'Cancelar';  
+	$lang['form_update_loading']	= 'Cargando, actualizando cambios...';
+	$lang['update_success_message']	= 'Los cambios fueron aplicados correctamente.';                
+	$lang['form_go_back_to_list']	= 'Regresar a la lista';
+
+	$lang['form_add']				= 'Agregar';
+	$lang['insert_success_message']	= 'Los datos fueron insertados correctamente en la Base de datos.';
+	$lang['form_or']				= 'o';
+	$lang['form_save']				= 'Guardar';
+	$lang['form_insert_loading']	= 'Cargando, guardando...';
+
+	$lang['form_upload_a_file']		= 'subir un archivo';
+	$lang['form_upload_delete']		= 'borrar';
+	$lang['form_button_clear']		= 'Limpiar';  
+
+	$lang['delete_success_message']	= 'Los datos fueron borrados correctamente en la Base de datos.';
+	$lang['delete_error_message']	= 'Los datos no pudieron ser borrados de la Base de datos.';
+
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Los datos que insertaste no pueden ser guardados.\\nEstas seguro que quieres regresar a la lista?';
+	$lang['alert_edit_form']		= 'Los datos que insertaste no han sido guardados.\\nEstas seguro que quieres regresar a la lista?';
+	$lang['alert_delete']			= 'Seguro que quieres borrar este registro?';
+
+	$lang['insert_error']			= 'Ocurrio un error durante la insercion.';
+	$lang['update_error']			= 'Ocurrio un error guardando.';
