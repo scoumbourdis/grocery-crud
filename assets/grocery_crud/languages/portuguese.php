8d5d72278d72cc8aa6d8e154eeaab22ab8df49a8
John Skoumbourdis <scoumbourdisj@gmail.com>
2011-12-28 03:33:02 +0200

    Adding Portuguese to the multilingual functionality - Portuguese translated by rafael84

---
+++ assets/grocery_crud/languages/portuguese.php
@@ -0,0 +1,50 @@
+﻿<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by rafael84 */
+	$lang['list_add'] 				= 'Adicionar';
+	$lang['list_actions'] 			= 'Ações';
+	$lang['list_page'] 				= 'Página';	
+	$lang['list_paging_of'] 		= 'de';
+	$lang['list_displaying']		= 'Registros de {start} a {end} total de {results} registros';
+	$lang['list_filtered_from']		= '(filtrado de {total_results} de entradas no total)';
+	$lang['list_show_entries']		= 'Exibir {paging} entradas';
+	$lang['list_no_items']			= 'Nenhum registro a ser exibido';
+	$lang['list_zero_entries']		= 'Nenhum registro encontrado';
+	$lang['list_search'] 			= 'Filtrar';	
+	$lang['list_search_all'] 		= '- Todos os Campos -';
+	$lang['list_clear_filtering'] 	= 'Limpar Filtro';
+	$lang['list_delete'] 			= 'Excluir';
+	$lang['list_edit'] 				= 'Alterar';	
+	$lang['list_paging_first'] 		= 'Primeiro';
+	$lang['list_paging_previous'] 	= 'Anterior';
+	$lang['list_paging_next'] 		= 'Próximo';
+	$lang['list_paging_last'] 		= 'Úlitmo';
+	$lang['list_loading'] 			= 'Carregando...';	
+
+	$lang['form_edit'] 				= 'Alterar';
+	$lang['form_back_to_list'] 		= 'Voltar para a Listagem';
+	$lang['form_update_changes'] 	= 'Aplicar alterações';
+	$lang['form_cancel'] 			= 'Cancelar';	
+	$lang['form_update_loading'] 	= 'Carregando, aplicando alterações...';
+	$lang['update_success_message'] = 'Os dados foram atualizados com sucesso.';	
+	$lang['form_go_back_to_list'] 	= 'Voltar para a Listagem';
+	
+	$lang['form_add'] 				= 'Adicionar';
+	$lang['insert_success_message'] = 'Os dados foram armazenados no banco de dados.';
+	$lang['form_or']				= 'ou';
+	$lang['form_save'] 				= 'Salvar';
+	$lang['form_insert_loading'] 	= 'Carregando, salvando dados...';
+	
+	$lang['form_upload_a_file'] 	= 'Enviar um Arquivo';
+	$lang['form_upload_delete'] 	= 'Remover';
+	$lang['form_button_clear'] 		= 'Limpar';	
+	
+	$lang['delete_success_message'] = 'O registro foi excluído do banco de dados.';
+	$lang['delete_error_message'] 	= 'O registro NÃO foi excluído do banco de dados.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Os dados que você inseriu NÃO foram salvos.\\nTem certeza que deseja retornar para a listagem?';
+	$lang['alert_edit_form']		= 'Os dados que você alterou podem NÃO ter sido salvos.\\nTem certeza que deseja retornar para a listagem?';
+	$lang['alert_delete']			= 'Tem certeza que deseja excluir este registro?';
+	
+	$lang['insert_error']			= 'Ocorreu um erro ao tentar gravar o registro.';
+	$lang['update_error']			= 'Ocorreu um erro ao atualizar o registro.';
