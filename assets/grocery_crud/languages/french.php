d0d9008f049bb6079d6e6cad081ffeb15be00cd6
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-01-02 22:56:25 +0000

    Adding French to the multilingual functionality - French translated by ASMAAYNI.

---
+++ assets/grocery_crud/languages/french.php
@@ -0,0 +1,50 @@
+<?php
+/* Translated by ASMAAYNI */
+	$lang['list_add'] 				= 'Ajouter';
+	$lang['list_actions'] 			= 'Actions';
+	$lang['list_page'] 				= 'Page';	
+	$lang['list_paging_of'] 		= 'de';
+	$lang['list_displaying']		= 'Affichage de {start} à {end} de {results} enregistrements';
+	$lang['list_filtered_from']		= '(filté de {total_results} entrées au total )';
+	$lang['list_show_entries']		= 'afficher {paging} entrées';
+	$lang['list_no_items']			= 'aucun enregistrement à afficher';
+	$lang['list_zero_entries']		= 'afficchage de 0 à 0 de 0 enregistrement';
+	$lang['list_search'] 			= 'Recherche';	
+	$lang['list_search_all'] 		= 'tous critères';
+	$lang['list_clear_filtering'] 	= 'Initialiser le filtrage';
+	$lang['list_delete'] 			= 'Supprimer';
+	$lang['list_edit'] 				= 'Editer';	
+	$lang['list_paging_first'] 		= 'Premier';
+	$lang['list_paging_previous'] 	= 'Précedent';
+	$lang['list_paging_next'] 		= 'Suivant';
+	$lang['list_paging_last'] 		= 'Dernier';
+	$lang['list_loading'] 			= 'En attente...';	
+
+	$lang['form_edit'] 				= 'Editer';
+	$lang['form_back_to_list'] 		= 'Retour à la liste';
+	$lang['form_update_changes'] 	= 'Sauvegarder les modifications';
+	$lang['form_cancel'] 			= 'Annuler';	
+	$lang['form_update_loading'] 	= 'En attente, mise à jour des changements...';
+	$lang['update_success_message'] = 'Vos données sont mises à jour avec succès.';	
+	$lang['form_go_back_to_list'] 	= 'Retourner à la liste';
+	
+	$lang['form_add'] 				= 'Ajouter';
+	$lang['insert_success_message'] = 'Vos données sont ajoutées à la base de données avec succès.';
+	$lang['form_or']				= 'ou';
+	$lang['form_save'] 				= 'Enregistrer';
+	$lang['form_insert_loading'] 	= 'En attente, Entregistrement des données...';
+	
+	$lang['form_upload_a_file'] 	= 'Chargeement d un fichier';
+	$lang['form_upload_delete'] 	= 'Supprimer';
+	$lang['form_button_clear'] 		= 'Initialiser';	
+	
+	$lang['delete_success_message'] = 'Vos données sont supprimées de la base de données avec succès.';
+	$lang['delete_error_message'] 	= 'Vos données ne sont pas supprimées de la base de données.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Les données que vous avez inserées ne sont pas enregistées.\\nêtes vous sûr de vouloir retourner à la liste?';
+	$lang['alert_edit_form']		= 'Les données que vous avez modifiées ne sont pas enregistées.\\nêtes vous sûr de vouloir retourner à la liste?';
+	$lang['alert_delete']			= 'êtes vous sûr de vouloir supprimer cet enregistrement?';
+	
+	$lang['insert_error']			= 'Une erreur est survenue lors de l opération d insersion.';
+	$lang['update_error']			= 'Une erreur est survenue lors de l opération d enregistrement.';
