dc78d7dd4ed31ae50c132caee87cc3f84c920312
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-01-07 15:59:05 +0000

    Adding Italian language. Tranlated by hunterbit.

---
+++ assets/grocery_crud/languages/italian.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by hunterbit */
+	$lang['list_add'] 				= 'Aggiungi';
+	$lang['list_actions'] 			= 'Azioni';
+	$lang['list_page'] 				= 'Pagina';	
+	$lang['list_paging_of'] 		= 'di';
+	$lang['list_displaying']		= 'Visualizza {start} alla {end} di {results} elementi';
+	$lang['list_filtered_from']		= '(filtrato dal {total_results} voci totali)';
+	$lang['list_show_entries']		= 'Visualizza {paging} voci';
+	$lang['list_no_items']			= 'No elementi da visualizzare';
+	$lang['list_zero_entries']		= 'Visualizzazione 0 a 0 di 0 elementi';
+	$lang['list_search'] 			= 'Ricerca';	
+	$lang['list_search_all'] 		= 'Ricerca tutto';
+	$lang['list_clear_filtering'] 	= 'Azzera filtri';
+	$lang['list_delete'] 			= 'Cancella';
+	$lang['list_edit'] 				= 'Modifica';	
+	$lang['list_paging_first'] 		= 'Primo';
+	$lang['list_paging_previous'] 	= 'Precedente';
+	$lang['list_paging_next'] 		= 'Prossimo';
+	$lang['list_paging_last'] 		= 'Ultimo';
+	$lang['list_loading'] 			= 'Caricamento in corso...';	
+
+	$lang['form_edit'] 				= 'Modifica';
+	$lang['form_back_to_list'] 		= 'Ritorna alla lista';
+	$lang['form_update_changes'] 	= 'Aggiorna modifiche';
+	$lang['form_cancel'] 			= 'Cancella';	
+	$lang['form_update_loading'] 	= 'Operazione in corso, aggiornamento modifiche...';
+	$lang['update_success_message'] = 'I tuoi dati sono stati aggiornati con successo.';	
+	$lang['form_go_back_to_list'] 	= 'Torna alla lista';
+	
+	$lang['form_add'] 				= 'Aggiungi';
+	$lang['insert_success_message'] = 'I tuoi dati sono stati salvati con successo nel database.';
+	$lang['form_or']				= 'o';
+	$lang['form_save'] 				= 'Salva';
+	$lang['form_insert_loading'] 	= 'Operazione in corso, salvataggio dati...';
+	
+	$lang['form_upload_a_file'] 	= 'Carica file';
+	$lang['form_upload_delete'] 	= 'cancella';
+	$lang['form_button_clear'] 		= 'Azzera';	
+	
+	$lang['delete_success_message'] = 'I tuoi dati sono stati cancellati con successo dal database.';
+	$lang['delete_error_message'] 	= 'I tuoi dati non sono stati cancellati dal database.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'I dati inseriti non possono essere salvati.\\nSei sicuro di voler ritornare alla lista?';
+	$lang['alert_edit_form']		= 'I dati modificati non possono essere salvati.\\nSei sicuro di voler ritornare alla lista?';
+	$lang['alert_delete']			= 'Sei sicuro di voler eliminare questo record?';
+	
+	$lang['insert_error']			= 'Un errore si è verifica in fase di Inserimento.';
+	$lang['update_error']			= 'Un errore si è verificato in fase di Salvataggio.';
