c4f0ef8fc25001821b12377c42bb62d4b0316554
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-01-21 11:07:36 +0000

    Adding Russian Language. Translated by noskov.biz

---
+++ assets/grocery_crud/languages/russian.php
@@ -0,0 +1,50 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*	Translated by @noskov.biz */
+	$lang['list_add'] = 'Добавить';
+	$lang['list_actions'] = 'Действия';
+	$lang['list_page'] = 'Страница';
+	$lang['list_paging_of'] = 'из';
+	$lang['list_displaying'] = 'Показано от {start} до {end} из {results} записей';
+	$lang['list_filtered_from'] = '(отфильтровано из {total_results} записей)';
+	$lang['list_show_entries'] = 'Показать {paging} записи';
+	$lang['list_no_items'] = 'Нет записей для отображения';
+	$lang['list_zero_entries'] = 'Показано от 0 до 0 из 0 записей';
+	$lang['list_search'] = 'Поиск';
+	$lang['list_search_all'] = 'Везде';
+	$lang['list_clear_filtering'] = 'Очистить фильтр';
+	$lang['list_delete'] = 'Удалить';
+	$lang['list_edit'] = 'Редактировать';
+	$lang['list_paging_first'] = 'Первая';
+	$lang['list_paging_previous'] = 'Предыдущая';
+	$lang['list_paging_next'] = 'Следующая';
+	$lang['list_paging_last'] = 'Последняя';
+	$lang['list_loading'] = 'Загрузка...';
+	
+	$lang['form_edit'] = 'Редактировать';
+	$lang['form_back_to_list'] = 'Вернуться к списку';
+	$lang['form_update_changes'] = 'Сохранить изменения';
+	$lang['form_cancel'] = 'Отмена';
+	$lang['form_update_loading'] = 'Загрузка, сохранение изменений...';
+	$lang['update_success_message'] = 'Информация успешно обновлена.';
+	$lang['form_go_back_to_list'] = 'Вернуться к списку';
+	
+	$lang['form_add'] = 'Добавить';
+	$lang['insert_success_message'] = 'Информация успешно добавлена в базу данных.';
+	$lang['form_or'] = 'или';
+	$lang['form_save'] = 'Сохранить';
+	$lang['form_insert_loading'] = 'Загрузка, сохранение информации...';
+	
+	$lang['form_upload_a_file'] = 'Загрузить файл';
+	$lang['form_upload_delete'] = 'Удалить';
+	$lang['form_button_clear'] = 'Очистить';
+	
+	$lang['delete_success_message'] = 'Информация успешно удалена из базы данных.';
+	$lang['delete_error_message'] = 'Информация не была удалена из базы данных.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form'] = 'Добавленная информация не сохранена.\\nВы уверены, что хотите вернуться к списку?';
+	$lang['alert_edit_form'] = 'Внесенные изменения не сохранены.\\nВы уверены, что хотите вернуться к списку?';
+	$lang['alert_delete'] = 'Вы уверены, что хотите удалить эту запись?';
+	
+	$lang['insert_error'] = 'При добавлении информации произошла ошибка.';
+	$lang['update_error'] = 'При сохранении информации произошла ошибка.';
