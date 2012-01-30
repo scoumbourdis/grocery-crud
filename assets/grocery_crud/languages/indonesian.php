439e0b428b9e56122c4c3d2f07440040740c5a44
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-01-30 21:19:41 +0000

    Adding languages: European Portuguese, Indonesian.

---
+++ assets/grocery_crud/languages/indonesian.php
@@ -0,0 +1,54 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	$lang['list_add'] 				= 'Tambah ';
+	$lang['list_actions'] 			= 'Aksi';
+	$lang['list_page'] 				= 'Hal';	
+	$lang['list_paging_of'] 		= 'dari ';
+	$lang['list_displaying']		= 'Halaman {start} - {end} dari: {results} item';
+	$lang['list_filtered_from']		= '(filtered from {total_results} total entries)';
+	$lang['list_show_entries']		= 'Ada {paging} data';
+	$lang['list_no_items']			= 'Tidak ada item';
+	$lang['list_zero_entries']		= 'Ditampilkan 0 - 0 dari 0 item';
+	$lang['list_search'] 			= 'Cari';	
+	$lang['list_search_all'] 		= 'Cari Semua';
+	$lang['list_clear_filtering'] 	= 'Hapus Pencarian';
+	$lang['list_delete'] 			= 'Hapus';
+	$lang['list_edit'] 				= 'Edit';	
+	$lang['list_view'] 				= 'Lihat';	
+	$lang['list_paging_first'] 		= 'First';
+	$lang['list_paging_previous'] 	= 'Previous';
+	$lang['list_paging_next'] 		= 'Next';
+	$lang['list_paging_last'] 		= 'Last';
+	$lang['list_loading'] 			= 'Tunggu...';	
+
+	$lang['form_edit'] 				= 'Edit';
+	$lang['form_view'] 				= 'Lihat';
+	$lang['form_back_to_list'] 		= 'Back to List';
+	$lang['form_update_changes'] 	= 'Simpan PPerubahan';
+	$lang['form_cancel'] 			= 'Batal';	
+	$lang['form_update_loading'] 	= 'Tunggu sesaat, data sedang dirubah...';
+	$lang['update_success_message'] = 'Sukses!, data telah disimpan.';	
+	$lang['form_go_back_to_list'] 	= 'Kembali ke menu';
+	
+	$lang['form_add'] 				= 'Tambah';
+	$lang['insert_success_message'] = 'Sukses!,data telah berhasil disimpan di database.';
+	$lang['form_or']				= 'or';
+	$lang['form_save'] 				= 'Save';
+	$lang['form_insert_loading'] 	= 'Tunggu sesaat, simpan data...';
+	
+	$lang['form_upload_a_file'] 	= 'Unggah berkas';
+	$lang['form_upload_delete'] 	= 'hapus';
+	$lang['form_button_clear'] 		= 'Bersihkan';	
+	
+	$lang['delete_success_message'] = 'Sukses!, data telah berhasil disimpan di database.';
+	$lang['delete_error_message'] 	= 'Maaf!, data tidak dapat dihapus dari database';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Data telah dimasukkan tetapi belum disimpan.\\nAnda yakin ingin kembali ke menu?';
+	$lang['alert_edit_form']		= 'Data telah dirubah tetapi belum disimpan.\\nAnda yakin ingin kembali ke menu?';
+	$lang['alert_delete']			= 'Anda yakin ingin menghapus data';
+	
+	$lang['insert_error']			= 'Error!, data tidak dapat dimasukkan.';
+	$lang['update_error']			= 'Error!, data tidak dapat disimpan.';
+
+	$lang['crud_previous']			= 'Kembali ke halaman sebelumnya';
+	$lang['crud_next']				= 'Lanjut ke halaman selanjutnya';
