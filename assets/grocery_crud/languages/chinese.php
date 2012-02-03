d02e152dcbc19b75be31d0af82de06d9201c903a
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-02-03 06:32:49 +0000

    Adding Chinese language.

---
+++ assets/grocery_crud/languages/chinese.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by @heenji */
+	$lang['list_add'] 				= '新增';
+	$lang['list_actions'] 			= '操作';
+	$lang['list_page'] 				= '页';	
+	$lang['list_paging_of'] 		= '/';
+	$lang['list_displaying']		= '显示 {start} 至 {end} /总计 {results} 记录';
+	$lang['list_filtered_from']		= '(筛选于 {total_results} 总记录)';
+	$lang['list_show_entries']		= '显示 {paging} 页记录';
+	$lang['list_no_items']			= '没有记录';
+	$lang['list_zero_entries']		= '显示 0 - 0 / 0 条记录';
+	$lang['list_search'] 			= '查询';	
+	$lang['list_search_all'] 		= '查询所有';
+	$lang['list_clear_filtering'] 	= '删除筛选条件';
+	$lang['list_delete'] 			= '删除';
+	$lang['list_edit'] 				= '编辑';	
+	$lang['list_paging_first'] 		= '首页';
+	$lang['list_paging_previous'] 	= '前一页';
+	$lang['list_paging_next'] 		= '后一页';
+	$lang['list_paging_last'] 		= '最后';
+	$lang['list_loading'] 			= '载入...';	
+
+	$lang['form_edit'] 				= '编辑';
+	$lang['form_back_to_list'] 		= '返回列表';
+	$lang['form_update_changes'] 	= '修改';
+	$lang['form_cancel'] 			= '取消';	
+	$lang['form_update_loading'] 	= '载入, 并更新修改...';
+	$lang['update_success_message'] = '修改成功！';	
+	$lang['form_go_back_to_list'] 	= '返回列表';
+	
+	$lang['form_add'] 				= '新增';
+	$lang['insert_success_message'] = '新增记录成功保存到数据库';
+	$lang['form_or']				= '或者';
+	$lang['form_save'] 				= '保存';
+	$lang['form_insert_loading'] 	= '载入,并保存...';
+	
+	$lang['form_upload_a_file'] 	= '上传文件';
+	$lang['form_upload_delete'] 	= '删除';
+	$lang['form_button_clear'] 		= '清空';	
+	
+	$lang['delete_success_message'] = '从数据库里成功删除你的数据.';
+	$lang['delete_error_message'] 	= '没有从数据库中删除你的数据.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= '你新增的数据可能没有保存.\\n确定返回列表吗?';
+	$lang['alert_edit_form']		= '你更新的数据可能没有保存.\\n确定返回列表吗??';
+	$lang['alert_delete']			= '你确定要删除这些记录?';
+	
+	$lang['insert_error']			= '新增记录的时候发生错误.';
+	$lang['update_error']			= '保存记录的时候发生错误.';
