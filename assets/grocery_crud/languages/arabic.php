27804aeb618b67b39068cfcf90f9c8082884b237
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-02-04 08:55:15 +0000

    Adding language: Arabic.

---
+++ assets/grocery_crud/languages/arabic.php
@@ -0,0 +1,52 @@
+﻿<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by Mohammed Yasser Moghrabiah */
+	$lang['list_add'] 				= 'اضافة';
+	$lang['list_actions'] 			= '';
+	$lang['list_page'] 				= 'صفحة';	
+	$lang['list_paging_of'] 		= 'من';
+	$lang['list_displaying']		= ' عرض من {start} الى {end}  |  من اصل {results} سطر';
+	$lang['list_filtered_from']		= '(مفلتر من أصل  {total_results} سطر)';
+	$lang['list_show_entries']		= 'عرض {paging} سطر';
+	$lang['list_no_items']			= 'لايوجد بيانات للعرض';
+	
+	$lang['list_zero_entries']		= 'عرض 0 الى 0 من 0 عنصر';
+	$lang['list_search'] 			= 'بحث';	
+	$lang['list_search_all'] 		= 'بحث بالكل';
+	$lang['list_clear_filtering'] 	= 'مسح فلاتر البحث';
+	$lang['list_delete'] 			= 'حذف';
+	$lang['list_edit'] 				= 'تعديل';	
+	$lang['list_paging_first'] 		= 'الاول';
+	$lang['list_paging_previous'] 	= 'السابق';
+	$lang['list_paging_next'] 		= 'التالي';
+	$lang['list_paging_last'] 		= 'الاخير';
+	$lang['list_loading'] 			= 'يتم التحميل...';	
+
+	$lang['form_edit'] 				= 'تعديل';
+	$lang['form_back_to_list'] 		= 'عودة -&gt;';
+	$lang['form_update_changes'] 	= 'تحديث التعديلات';
+	$lang['form_cancel'] 			= 'الغاء الأمر';	
+	$lang['form_update_loading'] 	= 'يتم تحديث البيانات...';
+	$lang['update_success_message'] = 'تم تحديث بياناتك بنجاح';	
+	$lang['form_go_back_to_list'] 	= 'العودة الى القائمة ';
+	
+	$lang['form_add'] 				= 'اضافة';
+	$lang['insert_success_message'] = 'تم حفظ بياناتك بنجاح';
+	$lang['form_or']				= 'أو';
+	$lang['form_save'] 				= 'حفظ';
+	$lang['form_insert_loading'] 	= 'يتم حفظ البيانات...';
+	
+	$lang['form_upload_a_file'] 	= 'رفع ملف';
+	$lang['form_upload_delete'] 	= 'حذف';
+	$lang['form_button_clear'] 		= 'مسح';	
+	
+	$lang['delete_success_message'] = 'تم حذف بياناتك بنجاح من قاعدة البيانات .';
+	$lang['delete_error_message'] 	= '. لم يتم حذف بياناتك من قاعدة البيانات';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= '. البيانات التي ادخلتها ربما لن تحفظ\\n هل انت متأكد انك تريد العودة ؟';
+	$lang['alert_edit_form']		= '. البيانات التي عدلتها ربما لن تحفظ\\n هل انت متأكد انك تريد العودة ؟';
+	$lang['alert_delete']			= ' هل انت متأكد انك تريد حذف بيانات السطر ؟';
+	
+	$lang['insert_error']			= '. حدث مشكلة عند ادخال البيانات';
+	$lang['update_error']			= '. حدث مشكلة عند تحديث البيانات';	
+	
