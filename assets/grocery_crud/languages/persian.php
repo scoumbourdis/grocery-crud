b3bfb5b52dc5b492443127852781a71126211cde
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-02-11 12:03:29 +0000

    Adding Language: Persian.

---
+++ assets/grocery_crud/languages/persian.php
@@ -0,0 +1,50 @@
+﻿<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	/* Translated by Moslem Mansouri www.hozn.ir */
+	$lang['list_add'] 				= 'افزودن';
+	$lang['list_actions'] 			= 'عمل';
+	$lang['list_page'] 				= 'صفحه';	
+	$lang['list_paging_of'] 		= 'از';
+	$lang['list_displaying']		= 'نمایش {start} تا {end} از {results} رکورد';
+	$lang['list_filtered_from']		= '(فیلتر از مجموع{total_results} رکورد)';
+	$lang['list_show_entries']		= 'نمایش {paging} رکورد';
+	$lang['list_no_items']			= 'چیزی برای نمایش وجود ندارد';
+	$lang['list_zero_entries']		= 'نمایش 0 تا 0 از 0 رکورد';
+	$lang['list_search'] 			= 'جستجو';	
+	$lang['list_search_all'] 		= 'جستجوی همه';
+	$lang['list_clear_filtering'] 	= 'بدون فیلتر';
+	$lang['list_delete'] 			= 'حذف';
+	$lang['list_edit'] 				= 'ویرایش';	
+	$lang['list_paging_first'] 		= 'اولین';
+	$lang['list_paging_previous'] 	= 'قبلی';
+	$lang['list_paging_next'] 		= 'بعدی';
+	$lang['list_paging_last'] 		= 'آخرین';
+	$lang['list_loading'] 			= 'درحال بارگزاری...';	
+
+	$lang['form_edit'] 				= 'ویرایش';
+	$lang['form_back_to_list'] 		= 'بازگشت به لیست';
+	$lang['form_update_changes'] 	= 'ذخیره تغییرات';
+	$lang['form_cancel'] 			= 'انصراف';	
+	$lang['form_update_loading'] 	= 'در حال, ذخیره تغییرات...';
+	$lang['update_success_message'] = 'اطلاعات با موفقیت ویرایش شدند.';	
+	$lang['form_go_back_to_list'] 	= 'بازگشت به لیست';
+	
+	$lang['form_add'] 				= 'افزودن';
+	$lang['insert_success_message'] = 'اطلاعات شما با موفقیت ارسال شد';
+	$lang['form_or']				= 'یا';
+	$lang['form_save'] 				= 'ذخیره';
+	$lang['form_insert_loading'] 	= 'درحال, ذخیره اطلاعات...';
+	
+	$lang['form_upload_a_file'] 	= 'افزودن فایل';
+	$lang['form_upload_delete'] 	= 'حذف';
+	$lang['form_button_clear'] 		= 'انصراف';	
+	
+	$lang['delete_success_message'] = 'اطلاعات شما با موفقیت  حذف شد.';
+	$lang['delete_error_message'] 	= 'در حذف اطلاعات مشکلی وجود دارد.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'اطلاعات شما ارسال نشده است.\\nمیخواهید به لیست برگردید؟';
+	$lang['alert_edit_form']		= 'اطلاعات شما ویرایش نشده است.\\nمیخواهید به لیست برگردید؟';
+	$lang['alert_delete']			= 'واقعا میخواهید این رکورد را حذف کنید؟';
+	
+	$lang['insert_error']			= 'در ارسال اطلاعات مشکلی وجود دارد.';
+	$lang['update_error']			= 'در ویرایش اطلاعات مشکلی وجود دارد.';
