16c99e274c17754cb01da41a2cd56ef8f78f5e75
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-03-09 19:01:44 +0000

    Adding language: Turkish.

---
+++ assets/grocery_crud/languages/turkish.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by Sinan Eldem - www.sinaneldem.com.tr */
+	$lang['list_add'] 				= 'Ekle';
+	$lang['list_actions'] 			= 'İşlemler';
+	$lang['list_page'] 				= 'Sayfa';	
+	$lang['list_paging_of'] 		= '#';
+	$lang['list_displaying']		= 'Toplam {results} kayıttan {start} ile {end} arası gösteriliyor';
+	$lang['list_filtered_from']		= '{total_results} toplam kayıttan filtrelendi';
+	$lang['list_show_entries']		= 'Sayfa başına {paging} kayıt göster';
+	$lang['list_no_items']			= 'Gösterilecek kayıt bulunamadı';
+	$lang['list_zero_entries']		= '0 kayıttan 0 ile 0 arası gösteriliyor';
+	$lang['list_search'] 			= 'Arama';	
+	$lang['list_search_all'] 		= 'Tümünü ara';
+	$lang['list_clear_filtering'] 	= 'Filtreyi temizle';
+	$lang['list_delete'] 			= 'Sil';
+	$lang['list_edit'] 				= 'Düzenle';	
+	$lang['list_paging_first'] 		= 'İlk';
+	$lang['list_paging_previous'] 	= 'Önceki';
+	$lang['list_paging_next'] 		= 'Sonraki';
+	$lang['list_paging_last'] 		= 'Son';
+	$lang['list_loading'] 			= 'Yükleniyor...';	
+
+	$lang['form_edit'] 				= 'Düzenle';
+	$lang['form_back_to_list'] 		= 'Listeye geri dön';
+	$lang['form_update_changes'] 	= 'Değişiklikleri uygula';
+	$lang['form_cancel'] 			= 'İptal';	
+	$lang['form_update_loading'] 	= 'Yükleniyor, değişliklikler uygulanıyor.';
+	$lang['update_success_message'] = 'Veri başarıyla güncellendi.';	
+	$lang['form_go_back_to_list'] 	= 'Listeye geri dön';
+	
+	$lang['form_add'] 				= 'Ekle';
+	$lang['insert_success_message'] = 'Veri başarıyla veritabanına kaydedildi.';
+	$lang['form_or']				= 'veya';
+	$lang['form_save'] 				= 'Kaydet';
+	$lang['form_insert_loading'] 	= 'Yükleniyor, veri kaydediliyor...';
+	
+	$lang['form_upload_a_file'] 	= 'Dosya yükle';
+	$lang['form_upload_delete'] 	= 'sil';
+	$lang['form_button_clear'] 		= 'Temizle';	
+	
+	$lang['delete_success_message'] = 'Kayıt başarıyla veritabanından kaldırıldı.';
+	$lang['delete_error_message'] 	= 'Kayıt veritabanından kaldırılamadı.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Girmiş olduğunuz veri eklenemedi veya kaydedilemedi.\\nListeye geri dönmek istediğinizden emin misiniz?';
+	$lang['alert_edit_form']		= 'Yapmış olduğunuz değişliklikler eklenemedi veya kaydedilemedi.\\nListeye geri dönmek istediğinizden emin misiniz?';
+	$lang['alert_delete']			= 'Bu kaydı silmek istediğinizden emin misiniz?';
+	
+	$lang['insert_error']			= 'Ekleme esnasında bir hata oluştu.';
+	$lang['update_error']			= 'Kaydetme esnasında bir hata oluştu.';
