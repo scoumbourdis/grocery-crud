66a9123d68247eef1142793a244d6344fbdbbab6
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-03-15 19:53:51 +0000

    Adding Language: Vietnamese

---
+++ assets/grocery_crud/languages/vietnamese.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by: @Vuong */
+	$lang['list_add'] 				= 'Thêm';
+	$lang['list_actions'] 			= 'Tác vụ';
+	$lang['list_page'] 				= 'Trang';	
+	$lang['list_paging_of'] 		= 'tổng';
+	$lang['list_displaying']		= 'Hiện thị từ {start} tới {end} trong tổng số {results}';
+	$lang['list_filtered_from']		= '(lọc từ tổng số {total_results} bản ghi)';
+	$lang['list_show_entries']		= 'Hiển thị {paging} bản ghi';
+	$lang['list_no_items']			= 'Không có gì để hiển thị';
+	$lang['list_zero_entries']		= '0 bản ghi';
+	$lang['list_search'] 			= 'Tìm kiếm';	
+	$lang['list_search_all'] 		= 'Tất cả';
+	$lang['list_clear_filtering'] 	= 'Xóa tìm kiếm';
+	$lang['list_delete'] 			= 'Xóa';
+	$lang['list_edit'] 				= 'Thay đổi';	
+	$lang['list_paging_first'] 		= 'Đầu tiên';
+	$lang['list_paging_previous'] 	= 'Trước đó';
+	$lang['list_paging_next'] 		= 'Tiếp theo';
+	$lang['list_paging_last'] 		= 'Cuối';
+	$lang['list_loading'] 			= 'Đang tải...';	
+
+	$lang['form_edit'] 				= 'Thay đổi';
+	$lang['form_back_to_list'] 		= 'Quay lại danh sách';
+	$lang['form_update_changes'] 	= 'Lưu thay đổi';
+	$lang['form_cancel'] 			= 'Hủy';	
+	$lang['form_update_loading'] 	= 'Đang thay đổi...';
+	$lang['update_success_message'] = 'Thay đổi thành công.';	
+	$lang['form_go_back_to_list'] 	= 'Quay lại danh sách';
+	
+	$lang['form_add'] 				= 'Thêm';
+	$lang['insert_success_message'] = 'Thêm thành công.';
+	$lang['form_or']				= 'hoặc';
+	$lang['form_save'] 				= 'Thêm';
+	$lang['form_insert_loading'] 	= 'Đang thêm...';
+	
+	$lang['form_upload_a_file'] 	= 'Tải file lên';
+	$lang['form_upload_delete'] 	= 'Xóa file';
+	$lang['form_button_clear'] 		= 'Xóa';	
+	
+	$lang['delete_success_message'] = 'Xóa thành công.';
+	$lang['delete_error_message'] 	= 'Dữ liệu chưa được xóa.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Dữ liệu thêm vào chưa được lưu.\\nBạn có chắc rằng muốn quay lại danh sách?';
+	$lang['alert_edit_form']		= 'Dữ liệu thay đổi chưa được lưu.\\nBạn có chắc rằng muốn quay lại danh sách?';
+	$lang['alert_delete']			= 'Bạn có chắc rằng muốn xóa bản ghi này?';
+	
+	$lang['insert_error']			= 'Có lỗi khi thêm dữ liệu.';
+	$lang['update_error']			= 'Có lỗi khi thay đổi dữ liệu.';
