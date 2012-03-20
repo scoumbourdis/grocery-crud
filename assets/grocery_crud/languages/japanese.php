96a373e1efc171fa728f45303969381702ac80df
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-03-20 21:07:43 +0000

    Adding Japanese language.

---
+++ assets/grocery_crud/languages/japanese.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by: Kenta Saito */
+	$lang['list_add'] 				= '挿入';
+	$lang['list_actions'] 			= '操作';
+	$lang['list_page'] 				= 'ページ';	
+	$lang['list_paging_of'] 		= '/';
+	$lang['list_displaying']		= '表示中の列 {start} - {end} ( 合計 {results} )';
+	$lang['list_filtered_from']		= '(合計 {total_results} から絞り込み)';
+	$lang['list_show_entries']		= '{paging} 件ずつ表示';
+	$lang['list_no_items']			= '返り値が空でした(行数0)';
+	$lang['list_zero_entries']		= '表示中の列 0 - 0 ( 合計 0 )';
+	$lang['list_search'] 			= '検索';	
+	$lang['list_search_all'] 		= '全て検索';
+	$lang['list_clear_filtering'] 	= '絞り込みをクリア';
+	$lang['list_delete'] 			= '削除';
+	$lang['list_edit'] 				= '編集';	
+	$lang['list_paging_first'] 		= '先頭';
+	$lang['list_paging_previous'] 	= '前';
+	$lang['list_paging_next'] 		= '次';
+	$lang['list_paging_last'] 		= '最後';
+	$lang['list_loading'] 			= '読み込み中';	
+
+	$lang['form_edit'] 				= '編集';
+	$lang['form_back_to_list'] 		= '一覧に戻る';
+	$lang['form_update_changes'] 	= '変更の保存';
+	$lang['form_cancel'] 			= 'キャンセル';	
+	$lang['form_update_loading'] 	= '読み込み中 (変更の保存)';
+	$lang['update_success_message'] = '変更が保存されました。';	
+	$lang['form_go_back_to_list'] 	= '一覧へ戻る';
+	
+	$lang['form_add'] 				= '挿入';
+	$lang['insert_success_message'] = '行が挿入されました。';
+	$lang['form_or']				= 'または';
+	$lang['form_save'] 				= '行の挿入';
+	$lang['form_insert_loading'] 	= '読み込み中 (行の挿入)';
+	
+	$lang['form_upload_a_file'] 	= 'ファイルをアップロード';
+	$lang['form_upload_delete'] 	= '削除';
+	$lang['form_button_clear'] 		= 'クリア';	
+	
+	$lang['delete_success_message'] = 'データが削除されました。';
+	$lang['delete_error_message'] 	= 'データを削除できませんでした。';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= '挿入データは保存されません。\\n本当に一覧に戻りますか?';
+	$lang['alert_edit_form']		= '変更データは保存されません。\\n本当に一覧に戻りますか?';
+	$lang['alert_delete']			= '本当に行を削除しますか?';
+	
+	$lang['insert_error']			= '行の挿入でエラーが発生しました。';
+	$lang['update_error']			= '変更の保存でエラーが発生しました。';
