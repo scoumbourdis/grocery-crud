ec8164f458e4f83ada3949942793bd3176beb917
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-03-21 22:59:22 +0000

    Adding Danish language.

---
+++ assets/grocery_crud/languages/danish.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by: @mikkelkaas */
+	$lang['list_add'] 				= 'Tilføj';
+	$lang['list_actions'] 			= 'Handlinger';
+	$lang['list_page'] 				= 'Side';	
+	$lang['list_paging_of'] 		= 'af';
+	$lang['list_displaying']		= 'Viser {start} til {end} af {results} række(r)';
+	$lang['list_filtered_from']		= '(ud af ialt {total_results} rækker)';
+	$lang['list_show_entries']		= 'Vis {paging} rækker';
+	$lang['list_no_items']			= 'Ingen rækker at vise';
+	$lang['list_zero_entries']		= 'Viser 0 til 0 af 0 rækker';
+	$lang['list_search'] 			= 'Søg';	
+	$lang['list_search_all'] 		= 'Søg alle';
+	$lang['list_clear_filtering'] 	= 'Fjern filtrering';
+	$lang['list_delete'] 			= 'Slet';
+	$lang['list_edit'] 				= 'Redigér';	
+	$lang['list_paging_first'] 		= 'Første';
+	$lang['list_paging_previous'] 	= 'Forrige';
+	$lang['list_paging_next'] 		= 'Næste';
+	$lang['list_paging_last'] 		= 'Sidste';
+	$lang['list_loading'] 			= 'Indlæser...';	
+
+	$lang['form_edit'] 				= 'Redigér';
+	$lang['form_back_to_list'] 		= 'Tilbage til liste';
+	$lang['form_update_changes'] 	= 'Opdatér ændringer';
+	$lang['form_cancel'] 			= 'Afbryd';	
+	$lang['form_update_loading'] 	= 'Indlæser, opdaterer ændringer...';
+	$lang['update_success_message'] = 'Dit data blev opdateret.';	
+	$lang['form_go_back_to_list'] 	= 'Gå tilbage til listen';
+	
+	$lang['form_add'] 				= 'Tilføj';
+	$lang['insert_success_message'] = 'Dit data blev gemt i databasen.';
+	$lang['form_or']				= 'eller';
+	$lang['form_save'] 				= 'Gem';
+	$lang['form_insert_loading'] 	= 'Indlæser, gemmer data...';
+	
+	$lang['form_upload_a_file'] 	= 'Upload en fil';
+	$lang['form_upload_delete'] 	= 'slet';
+	$lang['form_button_clear'] 		= 'Ryd';	
+	
+	$lang['delete_success_message'] = 'Dit data blev slettet fra databasen.';
+	$lang['delete_error_message'] 	= 'Dit data blev ikke slettet fra databasen.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Det data du har indsat bliver måske ikke gemt.\\nEr du sikker på du vil gå tilbage til listen?';
+	$lang['alert_edit_form']		= 'Det data du har ændret bliver måske ikke gemt.\\nEr du sikker på du vil gå tilbage til listen?';
+	$lang['alert_delete']			= 'Er du sikker på at du vil slette dette?';
+	
+	$lang['insert_error']			= 'Der opstod en fejl, da der blev indsat.';
+	$lang['update_error']			= 'Der opstod en fejl, da der blev gemt.';
