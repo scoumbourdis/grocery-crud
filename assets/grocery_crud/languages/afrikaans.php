0644d4ac46d510f0ae2efc7554291175b897af73
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-03-24 09:24:52 +0000

    Adding languages: Afrikaans, Romanian.

---
+++ assets/grocery_crud/languages/afrikaans.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by @KaBaDaBrA */
+	$lang['list_add'] 				= 'Voeg';
+	$lang['list_actions'] 			= 'Aksies';
+	$lang['list_page'] 				= 'Bladsy';	
+	$lang['list_paging_of'] 		= 'van';
+	$lang['list_displaying']		= 'Wys {start} na {end} van {results} items';
+	$lang['list_filtered_from']		= '(Gefiltreer uit {total_results} totale lyste)';
+	$lang['list_show_entries']		= 'Wys {paging} lyste';
+	$lang['list_no_items']			= 'Geen items om te vertoon';
+	$lang['list_zero_entries']		= 'Wys 0 tot 0 van 0 items';
+	$lang['list_search'] 			= 'Soek';	
+	$lang['list_search_all'] 		= 'Soek na als';
+	$lang['list_clear_filtering'] 	= 'Verwyder filtrering';
+	$lang['list_delete'] 			= 'Verwyder';
+	$lang['list_edit'] 				= 'Opdateer';	
+	$lang['list_paging_first'] 		= 'Eerste';
+	$lang['list_paging_previous'] 	= 'Vorige';
+	$lang['list_paging_next'] 		= 'Volgende';
+	$lang['list_paging_last'] 		= 'Laaste';
+	$lang['list_loading'] 			= 'Besig om te laai...';	
+
+	$lang['form_edit'] 				= 'Verander';
+	$lang['form_back_to_list'] 		= 'Terug na lys';
+	$lang['form_update_changes'] 	= 'Opdateer veranderinge';
+	$lang['form_cancel'] 			= 'Kanselleer';	
+	$lang['form_update_loading'] 	= 'Besig met die opdatering van veranderinge...';
+	$lang['update_success_message'] = 'Jou data was suksesvol geopgedateer.';	
+	$lang['form_go_back_to_list'] 	= 'Gaan terug na lys';
+
+	$lang['form_add'] 				= 'Voeg';
+	$lang['insert_success_message'] = 'Jou data was suksesvol in die databasis gestoor.';
+	$lang['form_or']				= 'of';
+	$lang['form_save'] 				= 'Stoor';
+	$lang['form_insert_loading'] 	= 'Besig om die data te stoor...';
+
+	$lang['form_upload_a_file'] 	= 'Laai &#39;n file';
+	$lang['form_upload_delete'] 	= 'verwyder';
+	$lang['form_button_clear'] 		= 'Herstel Form';	
+
+	$lang['delete_success_message'] = 'Jou data het suksesvol verwyder geword uit die databasis.';
+	$lang['delete_error_message'] 	= 'Jou data het nie suksesvol verwyder geword uit die databasis uit nie.';
+
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Die data wat jy het ingevoeg het kan nie gestoor word nie.\\nIs jy seker jy wil terug gaan na die lys toe?';
+	$lang['alert_edit_form']		= 'Die data wat jy verander het kan nie gestoor word nie.\\nIs jy seker jy wil terug gaan na die lys toe?';
+	$lang['alert_delete']			= 'Is jy seker jy wil van hierdie rekord ontslae raak?';
+
+	$lang['insert_error']			= 'Iets het vout geloop met die voeging.';
+	$lang['update_error']			= 'Iets het vout geloop met die stooring.';
