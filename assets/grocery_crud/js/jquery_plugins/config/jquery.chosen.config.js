5cfed3b585994ebb9e6599fcc8b378e81064ba41
John Skoumbourdis <scoumbourdisj@gmail.com>
2012-05-07 14:03:40 +0100

    Clearing some rubbish code.

--- assets/grocery_crud/js/jquery_plugins/config/jquery.chosen.config.js
+++ assets/grocery_crud/js/jquery_plugins/config/jquery.chosen.config.js
@@ -1,18 +1,3 @@
 $(function(){
-	$(".chosen-select,.chosen-multiple-select").chosen({allow_single_deselect:true});
-/*	
-	$(".ajax-chosen-select,.ajax-chosen-multiple-select").ajaxChosen({
-	    type: 'POST',
-	    url: ajax_relation_url,
-	    dataType: 'json'
-	}, function (data) {
-		    var terms = {};
-	
-		    $.each(data, function (i, val) {
-		        terms[i] = val;
-		    });
-	
-		    return terms;
-	});
-*/	
+	$(".chosen-select,.chosen-multiple-select").chosen({allow_single_deselect:true});	
 });
