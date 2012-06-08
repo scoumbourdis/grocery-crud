c3ebada82d81c6a788433ab897f5066daaf2b252
scoumbourdis <scoumbourdisj@gmail.com>
2012-06-08 07:15:33 +0100

    Adding Hungarian language.

---
+++ assets/grocery_crud/languages/hungarian.php
@@ -0,0 +1,75 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* Translated by: Wathfea */
+    $lang['list_add']               = 'Hozzáadás';
+    $lang['list_actions']           = 'Feladatok';
+    $lang['list_page']              = 'Oldal';   
+    $lang['list_paging_of']         = 'összesen';
+    $lang['list_displaying']        = '{start} elem a {end}-ből . Összesesen {results} elem';
+    $lang['list_filtered_from']     = '(filtered from {total_results} total entries)';
+    $lang['list_show_entries']      = '{paging} db mutatása';
+    $lang['list_no_items']          = 'Nincs megjelenítendő elem';
+    $lang['list_zero_entries']      = '0 elemből 0 elem mutatása';
+    $lang['list_search']            = 'Keresés'; 
+    $lang['list_search_all']        = 'Összes keresése';
+    $lang['list_clear_filtering']   = 'Szűrők tiltása';
+    $lang['list_delete']            = 'Törlés';
+    $lang['list_edit']              = 'Szerkesztés';   
+    $lang['list_paging_first']      = 'Első';
+    $lang['list_paging_previous']   = 'Előző';
+    $lang['list_paging_next']       = 'Következő';
+    $lang['list_paging_last']       = 'Utolsó';
+    $lang['list_loading']           = 'Betöltés...'; 
+
+    $lang['form_edit']              = 'Szerekszt';
+    $lang['form_back_to_list']      = 'Vissza a listához';
+    $lang['form_update_changes']    = 'Módosítás';
+    $lang['form_cancel']            = 'Kilépés'; 
+    $lang['form_update_loading']    = 'Betöltés, frissítések mentése...';
+    $lang['update_success_message'] = 'Az adatok sikeresen elmentve';   
+    $lang['form_go_back_to_list']   = 'Vissza a listához';
+    
+    $lang['form_add']               = 'Hozzáadás';
+    $lang['insert_success_message'] = 'Az adatok sikeresen elmentve az adatbázisba.';
+    $lang['form_or']                = 'vagy';
+    $lang['form_save']              = 'Mentés';
+    $lang['form_insert_loading']    = 'Betöltés, adatok mentése...';
+    
+    $lang['form_upload_a_file']     = 'Fájl feltöltése';
+    $lang['form_upload_delete']     = 'törlés';
+    $lang['form_button_clear']      = 'Ürít';  
+    
+    $lang['delete_success_message'] = 'Az adat sikeresen törölve az adatbázisból.';
+    $lang['delete_error_message']   = 'Az adatot nem sikerült törölni az adatbázisból';
+    
+    /* Javascript messages */
+    $lang['alert_add_form']         = 'A változásokat még nem mentette.\\nBiztos, hogy szeretne visszamenni a listához?';
+    $lang['alert_edit_form']        = 'A változásokat még nem mentette.\\nBiztos, hogy szeretne visszamenni a listához?';
+    $lang['alert_delete']           = 'Biztosan törölni szeretné ezt a bejegyzést?';
+    
+    $lang['insert_error']           = 'Hiba történt a beillesztés közben.';
+    $lang['update_error']           = 'Hiba történt mentés közben.';
+    
+    /* Added in version 1.2.1 */
+    $lang['set_relation_title']     = 'Kiválaszt {field_display_as}';
+    $lang['list_record']            = '';
+    $lang['form_inactive']          = 'inaktív';
+    $lang['form_active']            = 'aktív';
+    
+    /* Added in version 1.2.2 */
+    $lang['form_save_and_go_back']  = 'Mentés és vissza a listához';
+    $lang['form_update_and_go_back']= 'Felülír és vissza a listához';
+    
+    /* Added in version 1.2.2 */
+    $lang['form_save_and_go_back']  = 'Mentés és vissza a listához';
+    $lang['form_update_and_go_back']= 'Felülír és vissza a listához';
+
+    /* Upload functionality */
+    $lang['string_delete_file']     = "Fájl törlése";
+    $lang['string_progress']        = "Állapot: ";
+    $lang['error_on_uploading']     = "Feltöltés közben hiba történt";
+    $lang['message_promt_delete_file']  = "Biztosan törölni szeretné a fájlt?";
+    
+    $lang['error_max_number_of_files']  = "Egy időben csak egy fájlt tud feltölteni.";
+    $lang['error_accept_file_types']    = "Ilyen tipusú fájl feltöltése nem engedélyezett.";
+    $lang['error_max_file_size']        = "A feltölteni kívánt file mérete meghaladja a maximális fájl méretet: {max_file_size}";
+    $lang['error_min_file_size']        = "Üres fájlt nem tud feltölteni.";
