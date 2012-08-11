aef679510316817a90bdf3c71176113ec2bdf307
scoumbourdis <scoumbourdisj@gmail.com>
2012-08-11 12:52:28 +0100

    Adding fancybox JS to use it for future feature with uploading image.

---
+++ assets/grocery_crud/js/jquery_plugins/config/jquery.fancybox.config.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.image-thumbnail').fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});	
+});	
