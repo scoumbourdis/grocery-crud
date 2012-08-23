dd45a2436a6add7ae577fd00ba167630123ef470
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2012-08-23 11:51:45 +0700

    add where clause as the last parameter in set_relation_n_n

--- application/models/grocery_crud_model.php
+++ application/models/grocery_crud_model.php
@@ -401,10 +401,13 @@ class grocery_CRUD_Model  extends CI_Model  {
     
     function get_relation_n_n_unselected_array($field_info, $selected_values)
     {
+    	$use_where_clause = !empty($field_info->where_clause);
+    	
     	$select = "";
     	$related_field_title = $field_info->title_field_selection_table;
     	$use_template = strpos($related_field_title,'{') !== false;
     	$field_name_hash = $this->_unique_field_name($related_field_title);
+    	
     	if($use_template)
     	{
     		$related_field_title = str_replace(" ", "&nbsp;", $related_field_title);
@@ -415,6 +418,10 @@ class grocery_CRUD_Model  extends CI_Model  {
     		$select .= "$related_field_title as $field_name_hash";
     	}
     	$this->db->select('*, '.$select,false);
+    	
+    	if($use_where_clause){
+    		$this->db->where($field_info->where_clause);	
+    	}
     	
     	$selection_primary_key = $this->get_primary_key($field_info->selection_table);
         if(!$use_template)
