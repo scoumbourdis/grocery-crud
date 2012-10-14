c37379f14fc16aa6fc415a4aa4d3f362f821f5ad
scoumbourdis <scoumbourdisj@gmail.com>
2012-10-14 15:53:07 +0100

    Adding Slovak and Bengali language

---
+++ assets/grocery_crud/languages/bengali.php
@@ -0,0 +1,79 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	$lang['list_add'] 				= 'যোগ করুন';
+	$lang['list_actions'] 			= 'ক্রিয়া সমূহ';
+	$lang['list_page'] 				= 'পৃষ্ঠা';	
+	$lang['list_paging_of'] 		= 'এর';
+	$lang['list_displaying']		= 'দেখানো হয়েছে {start} থেকে {end} আইটেম {results} এর মধ্যে';
+	$lang['list_filtered_from']		= '(সব ডাটা থেকে {total_results} টি ডাটা ফিল্টার করা হয়েছে)';
+	$lang['list_show_entries']		= 'দেখানো হয়েছে {paging} ডাটা';
+	$lang['list_no_items']			= 'কোন আইটেম প্রদর্শন নাই';
+	$lang['list_zero_entries']		= 'দেখানো হয়েছে 0 থেকে 0 আইটেম 0 এর মধ্যে';
+	$lang['list_search'] 			= 'অনুসন্ধান';	
+	$lang['list_search_all'] 		= 'সকল অনুসন্ধান';
+	$lang['list_clear_filtering'] 	= 'ফিল্টার পরিস্কার করুন';
+	$lang['list_delete'] 			= 'মুছে ফেলুন';
+	$lang['list_edit'] 				= 'পুনরায় ঠিক করুন';	
+	$lang['list_paging_first'] 		= 'শুরু';
+	$lang['list_paging_previous'] 	= 'পূর্ববর্তী';
+	$lang['list_paging_next'] 		= 'তারপর';
+	$lang['list_paging_last'] 		= 'শেষ';
+	$lang['list_loading'] 			= 'অপেক্ষা করুন...';	 
+
+	$lang['form_edit'] 				= 'পুনরায় ঠিক করুন';
+	$lang['form_back_to_list'] 		= 'পিছনের তালিকায় এ যাওয়া'; 
+	$lang['form_update_changes'] 	= 'আপডেট করুন';
+	$lang['form_cancel'] 			= 'বাতিল করুন';	
+	$lang['form_update_loading'] 	= 'অপেক্ষা করুন, আপডেট পরিবর্তন হচ্ছে...';
+	$lang['update_success_message'] = 'আপনার তথ্য সফলভাবে আপডেট করা হয়েছে.';	
+	$lang['form_go_back_to_list'] 	= 'লিস্ট এ ফিরে যান';
+	
+	$lang['form_add'] 				= 'যোগ করুন';
+	$lang['insert_success_message'] = 'আপনার ডাটাবেস এর মধ্যে তথ্য সফলভাবে সংরক্ষিত হয়েছে';
+	$lang['form_or']				= 'অথবা';
+	$lang['form_save'] 				= 'সংরক্ষণ';
+	$lang['form_insert_loading'] 	= 'অপেক্ষা করুন,ডাটা সংরক্ষিত হচ্ছে...';
+	
+	$lang['form_upload_a_file'] 	= 'একটি ফাইল আপলোড করুন';
+	$lang['form_upload_delete'] 	= 'মুছে ফেলুন';
+	$lang['form_button_clear'] 		= 'পরিস্কার করুন';	
+	
+	$lang['delete_success_message'] = 'আপনার তথ্য ডাটাবেস থেকে সফলভাবে মুছে ফেলা হয়েছে.';
+	$lang['delete_error_message'] 	= 'আপনার তথ্য ডাটাবেস থেকে মোছা হয় নি.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'যে ডাটা আপনি যোগ করতে চেয়েছেন তা সংরক্ষিত হয় নি.\\nআপনি কি নিশ্চিত আপনি ফিরে যাও তালিকায় যেতে চান?';
+	$lang['alert_edit_form']		= 'যে ডাটা আপনি পরিবর্তন করতে চেয়েছেন তা সংরক্ষিত হয় নি.\\nআপনি কি নিশ্চিত আপনি ফিরে যাও তালিকায় যেতে চান?';
+	$lang['alert_delete']			= 'আপনি কি নিশ্চিত যে আপনি এই রেকর্ড মুছে দিতে চান?';
+	
+	$lang['insert_error']			= 'যোগ করার সময় একটি ত্রুটি হয়েছে.';
+	$lang['update_error']			= 'সংরক্ষণ করার সময় একটি ত্রুটি হয়েছে.';
+	
+	/* Added in version 1.2.1 */
+	$lang['set_relation_title']		= 'নির্বাচন করুন{field_display_as}';
+	$lang['list_record']			= 'রেকর্ড';
+	$lang['form_inactive']			= 'নিষ্ক্রয়';
+	$lang['form_active']			= 'সক্রিয়';
+	
+	/* Added in version 1.2.2 */
+	$lang['form_save_and_go_back']	= 'সংরক্ষণ করুন এবং তালিকায় ফিরে যান';
+	$lang['form_update_and_go_back']= 'আপডেট করুন এবং তালিকায় ফিরে যান';
+	
+	/* Added in version 1.2.2 */ 
+	$lang['form_save_and_go_back']	= 'সংরক্ষণ করুন এবং তালিকায় ফিরে যান';
+	$lang['form_update_and_go_back']= 'আপডেট করুন এবং তালিকায় ফিরে যান';
+ 
+	/* Upload functionality */
+	$lang['string_delete_file'] 	= "ফাইল মুছে ফেলার পদ্ধতি";
+	$lang['string_progress'] 		= "অগ্রগতি: ";
+	$lang['error_on_uploading'] 	= "একটি ত্রুটি ঘটেছে আপলোড এর সময়.";
+	$lang['message_prompt_delete_file'] = "আপনি কি নিশ্চিত যে আপনি এই ফাইলটি মুছে দিতে চান?";
+	
+	$lang['error_max_number_of_files'] 	= "আপনি শুধুমাত্র একটি ফাইল আপলোড করতে পারবেন প্রতিটি সময় এর জন্য.";
+	$lang['error_accept_file_types'] 	= "এই এক্সটেনশন আপলোড এর জন্য আপনার অনুমতি নাই.";
+	$lang['error_max_file_size'] 		= "আপলোড করা ফাইল টি ডিরেক্টিভ এ উল্লেখিত {max_file_size} ছাড়িয়ে গেছে.";
+	$lang['error_min_file_size'] 		= "আপনি একটি ফাঁকা ফাইল আপলোড করতে পারবেন না.";
+	
+	/* Added in version 1.3.1 */
+	$lang['list_export'] 	= "Export";
+	$lang['list_print'] 	= "Print";
+	$lang['minimize_maximize'] = 'Minimize/Maximize';	
