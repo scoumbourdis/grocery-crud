a014a23f037c3dc725f8cd159b56209f0b36c96a
scoumbourdis <scoumbourdisj@gmail.com>
2012-10-15 07:53:33 +0100

    Remove hacks for older versions.
    This was obviously a hack for the bug of CI. Hope it will never happen
    again.

--- application/models/grocery_crud_model.php
+++ application/models/grocery_crud_model.php
@@ -84,14 +84,7 @@ class grocery_CRUD_Model  extends CI_Model  {
     		
     	$this->db->select($select, false);    	
     	
-    	if(CI_VERSION == '2.1.0' || CI_VERSION == '2.1.1')//This hack is only for the release 2.1.1 that it seems that it has lot of bugs. They didn't even change the CI_VERSION to 2.1.1!
-    	{
-    		$results = $this->_hack_for_CI_2_1_1()->data;
-    	}
-    	else 
-    	{
-    		$results = $this->db->get($this->table_name)->result();
-    	}
+    	$results = $this->db->get($this->table_name)->result();
     	
     	return $results;
     }
@@ -101,44 +94,6 @@ class grocery_CRUD_Model  extends CI_Model  {
     	$table_name = $table_name === null ? $this->table_name : $table_name;
     	
     	$this->primary_keys[$table_name] = $field_name;
-    }
-    
-    protected function _hack_for_CI_2_1_1()
-    {
-    	//Quietly check if the query will be executed or not
-    	$temp_debug_value = $this->db->db_debug;
-    	$this->db->db_debug = false;
-    	
-    	$db_result = $this->db->get($this->table_name);
-    	
-    	//back to normal
-    	$this->db->db_debug = $temp_debug_value;
-    	
-    	/* Don't worry ONLY if the query doesn't execute correctly we will try for a second chance. */
-    	if($db_result === false)
-    	{
-    		/* I got you! Now we will try to execute the query with the normal way and without the bug of the CI */
-    		$query = str_replace("`","",$this->db->last_query());
-    		
-    		$results = array();
-    		/** 
-    		 * Second chance let's execute again the query with the old way as codeigniter add always AUTOMATICALLY grave_accent.  There is not another way to deal with this. I know this is an ugly solution but
-    		 * I couldn't figure out another solution without changing the core of CI. If anyone has another idea about this it will be appreciated.
-    		 */
-    		$db_query = mysql_query($query) or show_error("<b>Query failed:</b> ".mysql_error()."<br/><b>Actual query:</b> ".$query);
-    		
-    		while($result = mysql_fetch_object($db_query) ) {
-    			$results[] = $result;
-    		}
-    		
-    		$results = (object)array('data' => $results, 'num_rows' => mysql_num_rows($db_query));
-    		
-    		return $results;
-    	}
-    	else
-    	{
-    		return (object)array('data' => $db_result->result(), 'num_rows' => $db_result->num_rows());
-    	}
     }
     
     protected function relation_n_n_queries($select)
@@ -223,25 +178,12 @@ class grocery_CRUD_Model  extends CI_Model  {
     		
     		$this->db->select($select,false);
     		
-    		if(CI_VERSION == '2.1.0' || CI_VERSION == '2.1.1')//This hack is only for the release 2.1.1 that it seems that it has lot of bugs. They didn't even change the CI_VERSION to 2.1.1!
-    		{
-    			return $this->_hack_for_CI_2_1_1()->num_rows;
-    		}
-    		else
-    		{
-    			return $this->db->get($this->table_name)->num_rows();
-    		}    		
+    		return $this->db->get($this->table_name)->num_rows();
+    		    		
     	}
     	else 
     	{    	
-    		if(CI_VERSION == '2.1.0' || CI_VERSION == '2.1.1')//This hack is only for the release 2.1.1 that it seems that it has lot of bugs. They didn't even change the CI_VERSION to 2.1.1!
-    		{
-    			return $this->_hack_for_CI_2_1_1()->num_rows;
-    		}
-    		else
-    		{
-    			return $this->db->get($this->table_name)->num_rows();
-    		}
+    		return $this->db->get($this->table_name)->num_rows();
     	}
     }
     
