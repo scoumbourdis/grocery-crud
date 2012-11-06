c63719f60ed8eecf15c84cbd55792bc380a8c66e
noskov <sergey@noskov.biz>
2012-11-07 00:46:55 +0200

    Updatin CKEditor to v3.6.5 with minimum setup

--- assets/grocery_crud/texteditor/ckeditor/ckeditor_php4.php
+++ assets/grocery_crud/texteditor/ckeditor/ckeditor_php4.php
@@ -21,12 +21,12 @@ class CKEditor
 	 * The version of %CKEditor.
 	 * \private
 	 */
-	var $version = '3.6.3';
+	var $version = '3.6.5';
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 * \private
 	 */
-	var $_timestamp = 'C3HA5RM';
+	var $_timestamp = 'C9A85WF';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -80,7 +80,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	var $timestamp = "C3HA5RM";
+	var $timestamp = "C9A85WF";
 	/**
 	 * An array that holds event listeners.
 	 * \private
