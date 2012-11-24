05ecd00624b6f7cb8003a867027e369ae2c89932
scoumbourdis <scoumbourdisj@gmail.com>
2012-11-24 19:15:28 +0000

    Noty messages everywhere.

--- assets/grocery_crud/themes/datatables/js/datatables.js
+++ assets/grocery_crud/themes/datatables/js/datatables.js
@@ -146,24 +146,15 @@ function delete_row(delete_url , row_id)
 			{					
 				if(data.success)
 				{
-					$('#ajax_refresh_and_loading').trigger('click');
+					success_message(data.success_message);
 					
-					if ($('#report-success').is(":empty")) {
-						$('#report-success').html( data.success_message ).slideDown('slow');
-					} else {
-						$('#report-success').html( data.success_message ).fadeOut('fast').fadeIn('slow').fadeOut('fast').fadeIn('slow');
-					}					
-					
-					$('#report-error').html('').slideUp('fast');
 					$('tr#row-'+row_id).addClass('row_selected');
 					var anSelected = fnGetSelected( oTable );
 					oTable.fnDeleteRow( anSelected[0] );					
 				}
 				else
-				{
-					$('#report-error').html( data.error_message ).slideUp('fast').slideDown('slow');						
-					$('#report-success').html('').slideUp('fast');						
-					
+				{	
+					success_message(data.error_message);
 				}
 			}
 		});
