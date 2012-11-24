1a49a6b8eb37efc1f9e47d814d7b3fa41a47d135
scoumbourdis <scoumbourdisj@gmail.com>
2012-11-24 14:12:18 +0000

    jQuery noty to datatables edit.

--- assets/grocery_crud/themes/datatables/js/datatables-edit.js
+++ assets/grocery_crud/themes/datatables/js/datatables-edit.js
@@ -37,38 +37,28 @@ $(function(){
 									return true;
 								}								
 								
-								$('#report-error').hide().html('');									
-								$('.field_error').each(function(){
-									$(this).removeClass('field_error');
-								});									
+								$('.field_error').removeClass('field_error');
 								
-								$('#report-success').html(data.success_message);
-								$('#report-success').slideDown('slow');
+								success_message(data.success_message);
+								
 							}
 							else
 							{
-								alert(message_update_error);
+								error_message(message_update_error);
 							}
 						},
 						error: function(){
-								alert( message_update_error );
+							error_message( message_update_error );
 						}
 					});
 				}
 				else
 				{
-					$('.field_error').each(function(){
-						$(this).removeClass('field_error');
-					});
-					$('#report-error').slideUp('fast');
-					$('#report-error').html(data.error_message);
+					$('.field_error').removeClass('field_error');
+					error_message(data.error_message);
 					$.each(data.error_fields, function(index,value){
 						$('input[name='+index+']').addClass('field_error');
 					});
-							
-					$('#report-error').slideDown('normal');
-					$('#report-success').slideUp('fast').html('');
-					
 				}
 			}
 		});
