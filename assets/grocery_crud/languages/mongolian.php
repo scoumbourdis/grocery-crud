c3ac3b168fd2cac5f7aef3de55291b555d9c9bc2
erdenemunkh <ts.erka@gmail.com>
2012-12-25 19:24:44 -0800

    Create mongolian.php

---
+++ assets/grocery_crud/languages/mongolian.php
@@ -0,0 +1,49 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+  $lang['list_add']   		        = 'Нэмэх';
+	$lang['list_actions'] 			    = 'Үйлдлүүд';
+  $lang['list_page'] 			        = 'Хуудас';	
+	$lang['list_paging_of'] 		    = '-с';
+	$lang['list_displaying']		    = 'Нийт {results} бичлэг байна.';
+	$lang['list_filtered_from']	    = '(filtered from {total_results} total entries)';
+	$lang['list_show_entries']	    = 'Show {paging} entries';
+	$lang['list_no_items']			    = 'Дэлгэцэнд харуулах мэдээлэл байхгүй байна.';
+	$lang['list_zero_entries']	    = 'Нийт бичлэгийн тоо 0 байна.';
+	$lang['list_search'] 			      = 'Хайх';	
+	$lang['list_search_all'] 		    = 'Бүгдээс хайх';
+	$lang['list_clear_filtering']   = 'Шүүлтүүрийг арилгах';
+	$lang['list_delete'] 			      = 'Устгах';
+  $lang['list_edit'] 			        = 'Засах';	
+	$lang['list_paging_first'] 		  = 'Эхэнд';
+	$lang['list_paging_previous']   = 'Өмнөх';
+	$lang['list_paging_next'] 		  = 'Дараах';
+	$lang['list_paging_last'] 		  = 'Сүүлд';
+  $lang['list_loading'] 			    = 'Уншиж байна...';	
+
+	$lang['form_edit'] 			        = 'Засах';
+	$lang['form_back_to_list'] 		  = 'Жагсаалт руу буцах';
+	$lang['form_update_changes']    = 'Засах';
+	$lang['form_cancel'] 			      = 'Болих';	
+	$lang['form_update_loading']    = 'Мэдээллийг засаж байна. Түр хүлээнэ үү ...';
+	$lang['update_success_message'] = 'Амжилттай засагдлаа.';	
+	$lang['form_go_back_to_list']   = 'Жагсаалт руу буцах';
+	
+	$lang['form_add'] 			        = 'Нэмэх';
+	$lang['insert_success_message'] = 'Амжилттай хадгаллаа.';
+	$lang['form_or']			          = 'эсвэл';
+	$lang['form_save'] 			        = 'Хадгалах';
+	$lang['form_insert_loading']    = 'Мэдээллийг хадгалаж байна. Түр хүлээнэ үү ...';
+	
+	$lang['form_upload_a_file']     = 'Файл оруулах';
+	$lang['form_upload_delete']     = 'устгах';
+	$lang['form_button_clear'] 		  = 'Арилгах';	
+	
+	$lang['delete_success_message'] = 'Амжилттай устгалаа.';
+	$lang['delete_error_message']   = 'Устгаж чадсангүй.';
+	
+	/* Javascript messages */
+	$lang['alert_add_form']			    = 'Та энэ мэдээллийг хадгалахгүйгээр жагсаалт руу буцах уу?';
+	$lang['alert_edit_form']		    = 'Та энэ мэдээллийг хадгалахгүйгээр жагсаалт руу буцах уу?';
+	$lang['alert_delete']			      = 'Энэ бичлэгийг устгахдаа итгэлтэй байна уу?';
+	
+	$lang['insert_error']			      = 'Мэдээлэл оруулах үед алдаа гарлаа.';
+	$lang['update_error']			      = 'Мэдээлэл засах үед алдаа гарлаа.';
