b32a42def1e1225b7528384d5196519fb5186f34
Ashraf Kaabi <mail@kaabi.info>
2013-01-20 19:09:32 -0800

    Create jquery-ui-timepicker-ar.js
    Adding Arabic translating for time picker

---
+++ assets/grocery_crud/js/jquery_plugins/ui/i18n/timepicker/jquery-ui-timepicker-ar.js
@@ -0,0 +1,21 @@
+/* Arabic translation for the jQuery Timepicker Addon */
+/* Written by Ashraf Kaabi */
+(function($) {
+  $.timepicker.regional['ar'] = {
+		timeOnlyTitle: 'اختار الساعة',
+		timeText: 'الساعة',
+		hourText: 'الساعات',
+		minuteText: 'الدقائق',
+		secondText: 'الثواني',
+		millisecText: 'ميلي ثانية',
+		timezoneText: 'توقيت',
+		currentText: 'الآن',
+		closeText: 'الانتهاء',
+		timeFormat: 'hh:mm',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		ampm: false,
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['ar']);
+})(jQuery);
