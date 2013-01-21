44469858b91ec8d94569b1568409f5987cdfb63a
Ashraf Kaabi <mail@kaabi.info>
2013-01-20 19:15:17 -0800

    Create ui-multiselect-ar.js
    adding Arabic translation to multiselect

---
+++ assets/grocery_crud/js/jquery_plugins/ui/i18n/multiselect/ui-multiselect-ar.js
@@ -0,0 +1,12 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale ar, ar-eg
+ */
+/* Ashraf Kaabi -- mail@kaabi.info */
+
+$.extend($.ui.multiselect.locale, {
+  addAll:'اضافة الكل',
+	removeAll:'حذف الكل',
+	itemsCount:'الاختيارات المحددة'
+});
