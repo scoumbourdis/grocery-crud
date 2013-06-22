75b9f3f1c4c473e1297ac6ea265deef0788fe7b9
John Skoumbourdis <scoumbourdisj@gmail.com>
2013-06-22 11:23:29 +0100

    small comment changes.

--- application/config/grocery_crud.php
+++ application/config/grocery_crud.php
@@ -6,7 +6,7 @@
 	$config['grocery_crud_date_format']			= 'uk-date';
 
 	// The default per page when a user firstly see a list page
-	$config['grocery_crud_default_per_page']	= 25; //Can only take values 10,25,50,100
+	$config['grocery_crud_default_per_page']	= 25;
 
 	$config['grocery_crud_file_upload_allow_file_types'] 		= 'gif|jpeg|jpg|png|tiff|doc|docx|txt|odt|xls|xlsx|pdf|ppt|pptx|pps|ppsx|mp3|m4a|ogg|wav|mp4|m4v|mov|wmv|flv|avi|mpg|ogv|3gp|3g2';
 	$config['grocery_crud_file_upload_max_file_size'] 			= '20MB'; //ex. '10MB' (Mega Bytes), '1067KB' (Kilo Bytes), '5000B' (Bytes)
@@ -21,6 +21,7 @@
 
 	//All the forms are opening with dialog forms without refreshing the page once again.
 	$config['grocery_crud_dialog_forms'] = true;
-	
-	//All the forms are opening with dialog forms without refreshing the page once again.
-	$config['grocery_crud_paging_options'] = array('10','25','50','100');;
+
+	//Having some options at the list paging. This is the default one that all the websites are using.
+	//Make sure that the number of grocery_crud_default_per_page variable is included to this array.
+	$config['grocery_crud_paging_options'] = array('10','25','50','100');
