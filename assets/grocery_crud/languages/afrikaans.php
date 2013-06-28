ec3ae2245628a784e277e637964937571f4a2a70
Kobus Myburgh <kobus.myburgh@gmail.com>
2013-06-28 11:50:49 +0200

    Update afrikaans.php
    Oops. Previous translation dropped some of the later versions' strings. Added them now.

--- assets/grocery_crud/languages/afrikaans.php
+++ assets/grocery_crud/languages/afrikaans.php
@@ -2,51 +2,72 @@
 
 /* Original translation by @KaBaDaBrA, improved by Kobus Myburgh (@impero_consult) */
 
-	$lang['list_add'] 				= 'Voeg by';
-	$lang['list_actions'] 			= 'Aksies';
-	$lang['list_page'] 				= 'Bladsy';
-	$lang['list_paging_of'] 		= 'van';
-	$lang['list_displaying']		= 'Wys {start} tot {end} van {results} items';
-	$lang['list_filtered_from']		= '(Gefiltreer uit {total_results} totale lyste)';
-	$lang['list_show_entries']		= 'Wys {paging} lyste';
-	$lang['list_no_items']			= 'Geen items om te vertoon';
-	$lang['list_zero_entries']		= 'Wys 0 tot 0 van 0 items';
-	$lang['list_search'] 			= 'Soek';
-	$lang['list_search_all'] 		= 'Soek na alles';
-	$lang['list_clear_filtering'] 	= 'Verwyder filtrering';
-	$lang['list_delete'] 			= 'Verwyder';
-	$lang['list_edit'] 				= 'Dateer op';
-	$lang['list_paging_first'] 		= 'Eerste';
-	$lang['list_paging_previous'] 	= 'Vorige';
-	$lang['list_paging_next'] 		= 'Volgende';
-	$lang['list_paging_last'] 		= 'Laaste';
-	$lang['list_loading'] 			= 'Besig om te laai...';
+$lang['list_add']                   = 'Voeg by';
+$lang['list_actions']               = 'Aksies';
+$lang['list_page']                  = 'Bladsy';
+$lang['list_paging_of']             = 'van';
+$lang['list_displaying']            = 'Wys {start} tot {end} van {results} items';
+$lang['list_filtered_from']         = '(Gefiltreer uit {total_results} totale lyste)';
+$lang['list_show_entries']          = 'Wys {paging} lyste';
+$lang['list_no_items']              = 'Geen items om te vertoon';
+$lang['list_zero_entries']          = 'Wys 0 tot 0 van 0 items';
+$lang['list_search']                = 'Soek';
+$lang['list_search_all']            = 'Soek na alles';
+$lang['list_clear_filtering']       = 'Verwyder filtrering';
+$lang['list_delete']                = 'Verwyder';
+$lang['list_edit']                  = 'Dateer op';
+$lang['list_paging_first']          = 'Eerste';
+$lang['list_paging_previous']       = 'Vorige';
+$lang['list_paging_next']           = 'Volgende';
+$lang['list_paging_last']           = 'Laaste';
+$lang['list_loading']               = 'Besig om te laai...';
 
-	$lang['form_edit'] 				= 'Verander';
-	$lang['form_back_to_list'] 		= 'Terug na lys';
-	$lang['form_update_changes'] 	= 'Opdateer veranderinge';
-	$lang['form_cancel'] 			= 'Kanselleer';
-	$lang['form_update_loading'] 	= 'Besig met die opdatering van veranderinge...';
-	$lang['update_success_message'] = 'Jou data was suksesvol geopgedateer.';
-	$lang['form_go_back_to_list'] 	= 'Gaan terug na lys';
+$lang['form_edit']                  = 'Verander';
+$lang['form_back_to_list']          = 'Terug na lys';
+$lang['form_update_changes']        = 'Opdateer veranderinge';
+$lang['form_cancel']                = 'Kanselleer';
+$lang['form_update_loading']        = 'Besig met die opdatering van veranderinge...';
+$lang['update_success_message']     = 'Jou data was suksesvol geopgedateer.';
+$lang['form_go_back_to_list']       = 'Gaan terug na lys';
 
-	$lang['form_add'] 				= 'Voeg by';
-	$lang['insert_success_message'] = 'Jou data was suksesvol in die databasis gestoor.';
-	$lang['form_or']				= 'of';
-	$lang['form_save'] 				= 'Stoor';
-	$lang['form_insert_loading'] 	= 'Besig om die data te stoor...';
+$lang['form_add']                   = 'Voeg by';
+$lang['insert_success_message']     = 'Jou data was suksesvol in die databasis gestoor.';
+$lang['form_or']                    = 'of';
+$lang['form_save']                  = 'Stoor';
+$lang['form_insert_loading']        = 'Besig om die data te stoor...';
 
-	$lang['form_upload_a_file'] 	= 'Laai \'n lêer op';
-	$lang['form_upload_delete'] 	= 'Verwyder';
-	$lang['form_button_clear'] 		= 'Herstel vorm';
+$lang['form_upload_a_file']         = 'Laai \'n lêer op';
+$lang['form_upload_delete']         = 'Verwyder';
+$lang['form_button_clear']          = 'Herstel vorm';
 
-	$lang['delete_success_message'] = 'Jou data is suksesvol verwyder uit die databasis.';
-	$lang['delete_error_message'] 	= 'Jou data is nie suksesvol verwyder uit die databasis nie.';
+$lang['delete_success_message']     = 'Jou data is suksesvol verwyder uit die databasis.';
+$lang['delete_error_message']       = 'Jou data is nie suksesvol verwyder uit die databasis nie.';
 
-	/* Javascript messages */
-	$lang['alert_add_form']			= 'Die data wat jy het ingesleutel het kan nie gestoor word nie.\\nIs jy seker jy wil terug gaan na die lys toe?';
-	$lang['alert_edit_form']		= 'Die data wat jy verander het kan nie gestoor word nie.\\nIs jy seker jy wil terug gaan na die lys toe?';
-	$lang['alert_delete']			= 'Is jy seker jy wil hierdie rekord uitvee?';
+/* Javascript messages */
+$lang['alert_add_form']             = 'Die data wat jy het ingesleutel het kan nie gestoor word nie.\\nIs jy seker jy wil terug gaan na die lys toe?';
+$lang['alert_edit_form']            = 'Die data wat jy verander het kan nie gestoor word nie.\\nIs jy seker jy wil terug gaan na die lys toe?';
+$lang['alert_delete']               = 'Is jy seker jy wil hierdie rekord uitvee?';
 
-	$lang['insert_error']			= 'Iets het fout gegaan met die byvoeging.';
-	$lang['update_error']			= 'Iets het fout gegaan met die opdatering.';
+$lang['insert_error']               = 'Iets het fout geloop met die byvoeging.';
+$lang['update_error']               = 'Iets het fout geloop met die opdatering.';
+
+/* Added in version 1.2.1 */
+$lang['set_relation_title']         = 'Kies {field_display_as}';
+$lang['list_record']                = 'Rekord';
+$lang['form_inactive']              = 'onaktief';
+$lang['form_active']                = 'aktief';
+
+/* Added in version 1.2.2 */
+$lang['form_save_and_go_back']      = 'Stoor en keer terug na lys';
+$lang['form_update_and_go_back']    = 'Dateer op en keer terug na lys';
+
+/* Upload functionality */
+$lang['string_delete_file']         = "Besig om uit te vee";
+$lang['string_progress']            = "Vordering: ";
+$lang['error_on_uploading']         = "'n Fout het voorgekom met die oplaai proses.";
+$lang['message_prompt_delete_file'] = "Is jy seker jy wil hierdie lêer uitvee?";
+
+$lang['error_max_number_of_files']  = "Jy kan slegs een lêer op 'n slag oplaai.";
+$lang['error_accept_file_types']    = "Hierdie tipe agtervoegsel word nie toegelaat nie.";
+$lang['error_max_file_size']        = "Die opgelaaide lêer se grootte is meer as die maksium van {max_file_size}.";
+$lang['error_min_file_size']        = "Jy kan nie 'n leë lêer oplaai nie.";     
