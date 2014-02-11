09a1d17b079f1df5134cbc6137becf06b5ece52b
John Skoumbourdis <scoumbourdisj@gmail.com>
2014-02-11 23:30:26 +0000

    Change default Per page to 10

--- application/config/grocery_crud.php
+++ application/config/grocery_crud.php
@@ -6,7 +6,7 @@
 	$config['grocery_crud_date_format']			= 'uk-date';
 
 	// The default per page when a user firstly see a list page
-	$config['grocery_crud_default_per_page']	= 25;
+	$config['grocery_crud_default_per_page']	= 10;
 
 	$config['grocery_crud_file_upload_allow_file_types'] 		= 'gif|jpeg|jpg|png|tiff|doc|docx|txt|odt|xls|xlsx|pdf|ppt|pptx|pps|ppsx|mp3|m4a|ogg|wav|mp4|m4v|mov|wmv|flv|avi|mpg|ogv|3gp|3g2';
 	$config['grocery_crud_file_upload_max_file_size'] 			= '20MB'; //ex. '10MB' (Mega Bytes), '1067KB' (Kilo Bytes), '5000B' (Bytes)
