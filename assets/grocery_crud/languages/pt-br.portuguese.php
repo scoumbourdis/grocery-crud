573e96ca718b9284690f78912960e4a98a0d58a2
Jonathan André Schweder <jonathanschweder@gmail.com>
2014-06-09 07:55:17 -0300

    Corrected some words for pt-br.portuguese lang

--- assets/grocery_crud/languages/pt-br.portuguese.php
+++ assets/grocery_crud/languages/pt-br.portuguese.php
@@ -5,7 +5,7 @@
 	$lang['list_page'] 			= 'Página';	
 	$lang['list_paging_of'] 		= 'de';
 	$lang['list_displaying']		= 'Registros de {start} à {end}, num total de {results} registros';
-	$lang['list_filtered_from']		= '(filtrado de {total_results} de entradas no total)';
+	$lang['list_filtered_from']		= '(filtrado de um total de {total_results} registros)';
 	$lang['list_show_entries']		= 'Exibir {paging} entradas';
 	$lang['list_no_items']			= 'Nenhum registro a ser exibido';
 	$lang['list_zero_entries']		= 'Nenhum registro encontrado';
@@ -25,11 +25,11 @@
 	$lang['form_update_changes'] 		= 'Aplicar alterações';
 	$lang['form_cancel'] 			= 'Cancelar';	
 	$lang['form_update_loading'] 		= 'Carregando, aplicando alterações...';
-	$lang['update_success_message'] 	= 'Os dados foram atualizados com sucesso.';	
-	$lang['form_go_back_to_list'] 		= 'Voltar para a Listagem';
+	$lang['update_success_message'] 	= 'Os dados foram atualizados com sucesso';	
+	$lang['form_go_back_to_list'] 		= 'Voltar para a listagem';
 	
 	$lang['form_add'] 			= 'Adicionar';
-	$lang['insert_success_message'] 	= 'Os dados foram armazenados no banco de dados.';
+	$lang['insert_success_message'] 	= 'Os dados foram armazenados com sucesso';
 	$lang['form_or']			= 'ou';
 	$lang['form_save'] 			= 'Salvar';
 	$lang['form_insert_loading'] 		= 'Carregando, salvando dados...';
@@ -38,23 +38,23 @@
 	$lang['form_upload_delete'] 		= 'Remover';
 	$lang['form_button_clear'] 		= 'Limpar';	
 	
-	$lang['delete_success_message'] 	= 'O registro foi excluído do banco de dados.';
-	$lang['delete_error_message'] 		= 'O registro NÃO foi excluído do banco de dados.';
+	$lang['delete_success_message'] 	= 'O registro foi excluído com sucesso';
+	$lang['delete_error_message'] 		= 'O registro NÃO foi excluído';
 	
 	/* Javascript messages */
-	$lang['alert_add_form']			= 'Os dados que você inseriu NÃO foram salvos.\\nTem certeza que deseja retornar para a listagem?';
-	$lang['alert_edit_form']		= 'Os dados que você alterou podem NÃO ter sido salvos.\\nTem certeza que deseja retornar para a listagem?';
+	$lang['alert_add_form']			= 'Os dados que você inseriu NÃO foram salvos.\\nTem certeza que deseja retornar para a listagem ?';
+	$lang['alert_edit_form']		= 'Os dados que você alterou podem NÃO ter sido salvos.\\nTem certeza que deseja retornar para a listagem ?';
 	$lang['alert_delete']			= 'Tem certeza que deseja excluir este registro?';
 	
-	$lang['insert_error']			= 'Ocorreu um erro ao tentar gravar o registro.';
-	$lang['update_error']			= 'Ocorreu um erro ao atualizar o registro.';
+	$lang['insert_error']			= 'Ocorreu um erro ao tentar gravar o registro';
+	$lang['update_error']			= 'Ocorreu um erro ao atualizar o registro';
 	
 	/* Added in version 1.2.1 */
 	$lang['set_relation_title']		= 'Selecione {field_display_as}';
 	
 	$lang['list_record']			= 'Registro';
-	$lang['form_inactive']			= 'inativo';
-	$lang['form_active']			= 'ativo';
+	$lang['form_inactive']			= 'Inativo';
+	$lang['form_active']			= 'Ativo';
 	
 	/* Added in version 1.2.2 */
 	$lang['form_save_and_go_back']		= 'Salvar e voltar para a listagem';
@@ -63,13 +63,13 @@
 	/* Upload functionality */
 	$lang['string_delete_file'] 		= "Excluindo arquivo";
 	$lang['string_progress'] 		= "Progresso: ";
-	$lang['error_on_uploading'] 		= "Um erro foi encontrado ao tentar enviar o arquivo.";
-	$lang['message_prompt_delete_file'] 	= "Você tem certeza que deseja exluir esse arquivo?";
+	$lang['error_on_uploading'] 		= "Ocorreu um erro ao tentar enviar o arquivo";
+	$lang['message_prompt_delete_file'] 	= "Você tem certeza que deseja excluir esse arquivo?";
 	
-	$lang['error_max_number_of_files'] 	= "Você pode enviar apenas um arquivo por vez.";
-	$lang['error_accept_file_types'] 	= "Você não tem permissão para enviar esse tipo de arquivo.";
-	$lang['error_max_file_size'] 		= "O arquivo enviado excedeu o tamamnho máximo de {max_file_size}, especificado.";
-	$lang['error_min_file_size'] 		= "Você não pode enviar um arquivo vazio.";
+	$lang['error_max_number_of_files'] 	= "Você pode enviar apenas um arquivo por vez";
+	$lang['error_accept_file_types'] 	= "Você não tem permissão para enviar esse tipo de arquivo";
+	$lang['error_max_file_size'] 		= "O arquivo enviado excedeu o tamamnho máximo de {max_file_size}, especificado";
+	$lang['error_min_file_size'] 		= "Você não pode enviar um arquivo vazio";
 
 	/* Added in version 1.3.1 */
 	$lang['list_export'] 			= "Exportar";
