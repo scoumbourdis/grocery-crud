63b9cec3b1c614c0a96bce2a62f2eb6970fe31c4
Jonathan André Schweder <jonathanschweder@gmail.com>
2014-06-09 07:58:23 -0300

    Update pt-br.portuguese.php

--- assets/grocery_crud/languages/pt-br.portuguese.php
+++ assets/grocery_crud/languages/pt-br.portuguese.php
@@ -44,7 +44,7 @@
 	/* Javascript messages */
 	$lang['alert_add_form']			= 'Os dados que você inseriu NÃO foram salvos.\\nTem certeza que deseja retornar para a listagem ?';
 	$lang['alert_edit_form']		= 'Os dados que você alterou podem NÃO ter sido salvos.\\nTem certeza que deseja retornar para a listagem ?';
-	$lang['alert_delete']			= 'Tem certeza que deseja excluir este registro?';
+	$lang['alert_delete']			= 'Tem certeza que deseja excluir este registro ?';
 	
 	$lang['insert_error']			= 'Ocorreu um erro ao tentar gravar o registro';
 	$lang['update_error']			= 'Ocorreu um erro ao atualizar o registro';
@@ -64,7 +64,7 @@
 	$lang['string_delete_file'] 		= "Excluindo arquivo";
 	$lang['string_progress'] 		= "Progresso: ";
 	$lang['error_on_uploading'] 		= "Ocorreu um erro ao tentar enviar o arquivo";
-	$lang['message_prompt_delete_file'] 	= "Você tem certeza que deseja excluir esse arquivo?";
+	$lang['message_prompt_delete_file'] 	= "Você tem certeza que deseja excluir esse arquivo ?";
 	
 	$lang['error_max_number_of_files'] 	= "Você pode enviar apenas um arquivo por vez";
 	$lang['error_accept_file_types'] 	= "Você não tem permissão para enviar esse tipo de arquivo";
