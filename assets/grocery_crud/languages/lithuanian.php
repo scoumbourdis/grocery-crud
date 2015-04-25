4bb0877ee4a18fda230a2565cd57b66937d93d2b
John Skoumbourdis <scoumbourdisj@gmail.com>
2015-04-25 23:29:14 +0300

    Add lithuanian language, translated by: alex@gnosi.lt

---
+++ assets/grocery_crud/languages/lithuanian.php
@@ -0,0 +1,84 @@
+<?php
+/* Translated by alex@gnosi.lt */
+$lang['list_add'] = 'Pridėti';
+$lang['list_actions'] = 'Veiksmai';
+$lang['list_page'] = 'Puslapis';
+$lang['list_paging_of'] = 'iš';
+$lang['list_displaying'] = 'Rodoma nuo {start} iki {end} iš {results} užrašų';
+$lang['list_filtered_from'] = '(išfiltruota iš {total_results} užrašų)';
+$lang['list_show_entries'] = 'Rodyti {paging} užrašų';
+$lang['list_no_items'] = 'Nera įrašų atvaizdavimui';
+$lang['list_zero_entries'] = 'Rodoma 0 įrašų, nes yra 0 įrašų.';
+$lang['list_search'] = 'Paieška';
+$lang['list_search_all'] = 'Ieškoti visur';
+$lang['list_clear_filtering'] = 'Valyti filtrą';
+$lang['list_delete'] = 'Trinti';
+$lang['list_edit'] = 'Redaguoti';
+$lang['list_paging_first'] = 'Pirmas';
+$lang['list_paging_previous'] = 'Prieštai esantis';
+$lang['list_paging_next'] = 'Sekantis';
+$lang['list_paging_last'] = 'Paskutinis';
+$lang['list_loading'] = 'Kraunama...';
+
+$lang['form_edit'] = 'Redaguoti';
+$lang['form_back_to_list'] = 'Atgal į sąrašą';
+$lang['form_update_changes'] = 'Saugoti pakeitimus';
+$lang['form_cancel'] = 'Atšaukti';
+$lang['form_update_loading'] = 'Kraunama, informacijos atnaujinimas...';
+$lang['update_success_message'] = 'Info sekmingai atnaujinta.';
+$lang['form_go_back_to_list'] = 'Grįžti prie sąrašo';
+
+$lang['form_add'] = 'Pridėti';
+$lang['insert_success_message'] = 'Info sekmingai įrašyta į Duomenų bazę.';
+$lang['form_or'] = 'arba';
+$lang['form_save'] = 'saugoti';
+$lang['form_insert_loading'] = 'Kraunama, info saugoma...';
+
+$lang['form_upload_a_file'] = 'Atsiųsti bylą';
+$lang['form_upload_delete'] = 'Trinti';
+$lang['form_button_clear'] = 'Valyti';
+
+$lang['delete_success_message'] = 'Info išvalyta iš duomenų bazės.';
+$lang['delete_error_message'] = 'Info neišvalyta is Duomenų bazės.';
+
+/* Javascript messages */
+$lang['alert_add_form'] = 'Įrašyta info gali būti neišsaugota.\\nNorite grįžti prie sąrašo?';
+$lang['alert_edit_form'] = 'Pakeista info gali būti neišsaugota.\\nNorite grįžti prie sąrašo?';
+$lang['alert_delete'] = 'Ar tikrai noriti ištrinti įrašą?';
+
+$lang['insert_error'] = 'Įrašant įvyko klaida.';
+$lang['update_error'] = 'Įrašant įvyko klaida.';
+
+/* Added in version 1.2.1 */
+$lang['set_relation_title'] = 'pasirinkti {field_display_as}';
+$lang['list_record'] = 'įrašas';
+$lang['form_inactive'] = 'pasyvu';
+$lang['form_active'] = 'aktyvu';
+
+/* Added in version 1.2.2 */
+$lang['form_save_and_go_back'] = 'Saugoti ir grįžti į sąrašą';
+$lang['form_update_and_go_back']= 'Saugoti pakeitimus ir grįžti į sąrašą';
+
+/* Upload functionality */
+$lang['string_delete_file'] = 'Bylos šalinimas';
+$lang['string_progress'] = 'Progresas: ';
+$lang['error_on_uploading'] = 'Ivyko klaida, byla neužkrauta.';
+$lang['message_prompt_delete_file'] = 'Ar Jus tikrai norite ištrinti šia bylą?';
+
+$lang['error_max_number_of_files'] = 'Vienu karti galite užkrauti tik viena bylą.';
+$lang['error_accept_file_types'] = 'Negalima užkrauti bylos, dėl bylos fotmato.';
+$lang['error_max_file_size'] = 'Byla labai didelė, negalima jos užgrauti. Dydis: {max_file_size}.';
+$lang['error_min_file_size'] = 'Negalima užkrauti tuščios bylos.';
+
+/* Added in version 1.3.1 */
+$lang['list_export'] = 'Eksportuoti';
+$lang['list_print'] = 'Spausdinti';
+$lang['minimize_maximize'] = 'Sumažinti/Padidinti';
+
+/* Added in version 1.4 */
+$lang['list_view'] = 'Pažiūrėti';
+
+/* Added in version 1.5.1 */
+$lang['ui_day'] = 'dd';
+$lang['ui_month'] = 'mm';
+$lang['ui_year'] = 'yyyy';
