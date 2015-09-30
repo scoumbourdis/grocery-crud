1e2b5f9ee64aab9959a31b539ce0a3cda0a151bb
Martin Hering <nekomajin@gmx.de>
2015-09-30 09:51:13 +0200

    change formatting, added german translation, convert indentation to spaces
    - change formatting, so all strings stands in a column
    - added german translations
    - convert indentation to spaces

--- assets/grocery_crud/languages/german.php
+++ assets/grocery_crud/languages/german.php
@@ -1,88 +1,87 @@
 <?php
-	$lang['list_add'] 				= 'Neu:';
-	$lang['list_actions'] 			= 'Aktionen';
-	$lang['list_page'] 				= 'Seite';
-	$lang['list_paging_of'] 		= 'von';
-	$lang['list_displaying']		= 'Zeige {start} bis {end} von {results} Einträgen';
-	$lang['list_filtered_from']		= '(gefiltert aus {total_results} Einträgen insgesamt)';
-	$lang['list_show_entries']		= 'Zeige {paging} Einträge';
-	$lang['list_no_items']			= 'keine Einträge vorhanden';
-	$lang['list_zero_entries']		= 'Zeige 0 bis 0 von 0 Einträgen';
-	$lang['list_search'] 			= 'Suche';
-	$lang['list_search_all'] 		= 'Zeige alle';
-	$lang['list_clear_filtering'] 	= 'ohne Filter';
-	$lang['list_delete'] 			= 'Löschen';
-	$lang['list_edit'] 				= 'Ändern';
-	$lang['list_paging_first'] 		= 'Erster';
-	$lang['list_paging_previous'] 	= 'Vorheriger';
-	$lang['list_paging_next'] 		= 'Nächster';
-	$lang['list_paging_last'] 		= 'Letzter';
-	$lang['list_loading'] 			= 'Lade...';
+    $lang['list_add']                   = 'Neu:';
+    $lang['list_actions']               = 'Aktionen';
+    $lang['list_page']                  = 'Seite';
+    $lang['list_paging_of']             = 'von';
+    $lang['list_displaying']            = 'Zeige {start} bis {end} von {results} Einträgen';
+    $lang['list_filtered_from']         = '(gefiltert aus {total_results} Einträgen insgesamt)';
+    $lang['list_show_entries']          = 'Zeige {paging} Einträge';
+    $lang['list_no_items']              = 'keine Einträge vorhanden';
+    $lang['list_zero_entries']          = 'Zeige 0 bis 0 von 0 Einträgen';
+    $lang['list_search']                = 'Suche';
+    $lang['list_search_all']            = 'Zeige alle';
+    $lang['list_clear_filtering']       = 'ohne Filter';
+    $lang['list_delete']                = 'Löschen';
+    $lang['list_edit']                  = 'Ändern';
+    $lang['list_paging_first']          = 'Erster';
+    $lang['list_paging_previous']       = 'Vorheriger';
+    $lang['list_paging_next']           = 'Nächster';
+    $lang['list_paging_last']           = 'Letzter';
+    $lang['list_loading']               = 'Lade...';
 
-	$lang['form_edit'] 				= 'Ändern';
-	$lang['form_back_to_list'] 		= 'Zurück zur Liste';
-	$lang['form_update_changes'] 	= 'Änderungen speichern';
-	$lang['form_cancel'] 			= 'Abbrechen';
-	$lang['form_update_loading'] 	= 'Lade, speichere Änderungen...';
-	$lang['update_success_message'] = 'Daten erfolgreich aktualisiert.';
-	$lang['form_go_back_to_list'] 	= 'Gehe zurück zur Liste';
+    $lang['form_edit']                  = 'Ändern';
+    $lang['form_back_to_list']          = 'Zurück zur Liste';
+    $lang['form_update_changes']        = 'Änderungen speichern';
+    $lang['form_cancel']                = 'Abbrechen';
+    $lang['form_update_loading']        = 'Ladevorgang. Änderungen werden gespeichert...';
+    $lang['update_success_message']     = 'Daten wurden erfolgreich aktualisiert.';
+    $lang['form_go_back_to_list']       = 'Gehe zurück zur Liste';
 
-	$lang['form_add'] 				= 'Neu:';
-	$lang['insert_success_message'] = 'Erfolgreich in die Datenbank eingetragen!';
-	$lang['form_or']				= 'oder';
-	$lang['form_save'] 				= 'Speichern';
-	$lang['form_insert_loading'] 	= 'Lade, speicher Daten...';
+    $lang['form_add']                   = 'Neu:';
+    $lang['insert_success_message']     = 'Daten wurden erfolgreich in die Datenbank eingetragen!';
+    $lang['form_or']                    = 'oder';
+    $lang['form_save']                  = 'Speichern';
+    $lang['form_insert_loading']        = 'Ladevorgang. Daten werden gespeichert...';
 
-	$lang['form_upload_a_file'] 	= 'Datei hochladen';
-	$lang['form_upload_delete'] 	= 'löschen';
-	$lang['form_button_clear'] 		= 'Leeren';
+    $lang['form_upload_a_file']         = 'Datei hochladen';
+    $lang['form_upload_delete']         = 'löschen';
+    $lang['form_button_clear']          = 'Leeren';
 
-	$lang['delete_success_message'] = 'Datensatz erfolgreich aus der Datenbank gelöscht';
-	$lang['delete_error_message'] 	= 'Datensatz wurde nicht gelöscht.';
+    $lang['delete_success_message']     = 'Datensatz wurde erfolgreich aus der Datenbank gelöscht.';
+    $lang['delete_error_message']       = 'Datensatz wurde nicht gelöscht.';
 
-	/* Javascript messages */
-	$lang['alert_add_form']			= 'Die Daten sind noch nicht gespeichert.\\nSicher, dass Sie zurück zur Liste wollen?';
-	$lang['alert_edit_form']		= 'Die Korrektur ist noch nicht gespeichert.\\nSicher, dass Sie zurück zur Liste wollen?';
-	$lang['alert_delete']			= 'Sind Sie wirklich sicher, dass Sie das löschen wollen und dürfen?';
+    /* Javascript messages */
+    $lang['alert_add_form']             = 'Die Daten wurden noch nicht gespeichert.\\nSind Sie sicher, dass Sie zurück zur Liste wollen?';
+    $lang['alert_edit_form']            = 'Die Korrektur wurde noch nicht gespeichert.\\nSind Sie sicher, dass Sie zurück zur Liste wollen?';
+    $lang['alert_delete']               = 'Sind Sie wirklich sicher, dass Sie diesen Datensatz löschen wollen?';
 
-	$lang['insert_error']			= 'Bei dem Eintrag ist ein Fehler unterlaufen. Bitte prüfen.';
-	$lang['update_error']			= 'Ein Fehler beim Speichern. Bitte prüfen.';
+    $lang['insert_error']               = 'Beim Eintrag in die Datenbank ist ein Fehler aufgetreten. Bitte prüfen.';
+    $lang['update_error']               = 'Beim Speichern ist ein Fehler aufgetreten. Bitte prüfen.';
 
-	/* Added in version 1.2.1 */
-	$lang['set_relation_title']		= '{field_display_as} wählen';
-	$lang['list_record']			= 'Eintrag';
-	$lang['form_inactive']			= 'inaktiv';
-	$lang['form_active']			= 'aktiv';
+    /* Added in version 1.2.1 */
+    $lang['set_relation_title']         = '{field_display_as} wählen';
+    $lang['list_record']                = 'Eintrag';
+    $lang['form_inactive']              = 'inaktiv';
+    $lang['form_active']                = 'aktiv';
 
-	/* Added in version 1.2.2 */
-	$lang['form_save_and_go_back'] = 'Speichern und zur Übersicht';
-	$lang['form_update_and_go_back']= 'Aktualisieren und zur Übersicht';
+    /* Added in version 1.2.2 */
+    $lang['form_save_and_go_back']      = 'Speichern und zur Übersicht';
+    $lang['form_update_and_go_back']    = 'Aktualisieren und zur Übersicht';
 
-	/* Upload functionality */
-	$lang['string_delete_file'] = "Datei löschen";
-	$lang['string_progress'] = "Fortschritt: ";
-	$lang['error_on_uploading'] = "Beim Hochladen ist ein Fehler aufgetreten.";
-	$lang['message_prompt_delete_file'] = "Sind Sie sicher, dass Sie die Datei löschen wollen?";
+    /* Upload functionality */
+    $lang['string_delete_file']         = "Datei löschen";
+    $lang['string_progress']            = "Fortschritt: ";
+    $lang['error_on_uploading']         = "Beim Hochladen ist ein Fehler aufgetreten.";
+    $lang['message_prompt_delete_file'] = "Sind Sie sicher, dass Sie die Datei löschen wollen?";
 
-	$lang['error_max_number_of_files'] = "Sie können nur eine Datei gleicheitig hochladen.";
-	$lang['error_accept_file_types'] = "Der Dateityp ist unzulässig";
-	$lang['error_max_file_size'] = "Die hochgeladene Datei Übersteigt die maximale Dateigröße.";
-	$lang['error_min_file_size'] = "Leere Dateien können nicht hochgeladen werden";
+    $lang['error_max_number_of_files']  = "Sie können nur eine Datei je Vorgang hochladen.";
+    $lang['error_accept_file_types']    = "Der Dateityp ist unzulässig.";
+    $lang['error_max_file_size']        = "Die hochgeladene Datei übersteigt die maximale Dateigröße.";
+    $lang['error_min_file_size']        = "Leere Dateien können nicht hochgeladen werden.";
 
-	/* Added in version 1.3.1 */
-	$lang['list_export'] 	= "Export";
-	$lang['list_print'] 	= "Druck";
-	$lang['minimize_maximize'] = 'Minimieren/Maximieren';
+    /* Added in version 1.3.1 */
+    $lang['list_export']                = "Export";
+    $lang['list_print']                 = "Drucken";
+    $lang['minimize_maximize']          = 'Minimieren/Maximieren';
 
+    /* Added in version 1.4 */
+    $lang['list_view']                  = 'Anzeigen';
 
-	/* Added in version 1.4 */
-	$lang['list_view'] = 'Anzeigen';
+    /* Added in version 1.5.1 */
+    $lang['ui_day']                     = 'TT';
+    $lang['ui_month']                   = 'MM';
+    $lang['ui_year']                    = 'JJJJ';
 
-	/* Added in version 1.5.1 */
-	$lang['ui_day'] = 'dd';
-	$lang['ui_month'] = 'mm';
-	$lang['ui_year'] = 'yyyy';
-
-	/* Added in version 1.5.2 */
-	$lang['list_more'] = 'More';
+    /* Added in version 1.5.2 */
+    $lang['list_more']                  = 'Mehr';
 
