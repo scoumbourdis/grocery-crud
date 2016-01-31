ef59a6126d9cd689726f004e2b9f290eb8a41357
John Skoumbourdis <scoumbourdisj@gmail.com>
2016-01-31 21:56:37 +0200

    Add Croatian language from: Siniša Dragičević Martinčić

---
+++ assets/grocery_crud/languages/croatian.php
@@ -0,0 +1,91 @@
+<?php
+	$lang['list_add'] 				= 'Dodaj';
+	$lang['list_actions'] 			= 'Akcije';
+	$lang['list_page'] 				= 'Stranica';
+	$lang['list_paging_of'] 		= 'od';
+	$lang['list_displaying']		= 'Prikazujem {start} da {end} od {results} stavki';
+	$lang['list_filtered_from']		= '(filtrirano iz  {total_results} zapisa ukupno)';
+	$lang['list_show_entries']		= 'Prikaži {paging} unose';
+	$lang['list_no_items']			= 'ne postoji unos za prikaz';
+	$lang['list_zero_entries']		= 'Prikazano 0 do 0 od 0 unosa';
+	$lang['list_search'] 			= 'Pretraži';
+	$lang['list_search_all'] 		= 'Pretraži sve';
+	$lang['list_clear_filtering'] 	= 'Poništi filtriranje';
+	$lang['list_delete'] 			= 'Izbriši';
+	$lang['list_edit'] 				= 'Uredi';
+	$lang['list_paging_first'] 		= 'Prva';
+	$lang['list_paging_previous'] 	= 'Slijedeća';
+	$lang['list_paging_next'] 		= 'Iduća';
+	$lang['list_paging_last'] 		= 'Zadnja';
+	$lang['list_loading'] 			= 'Učitavanje...';
+
+	$lang['form_edit'] 				= 'Uredi';
+	$lang['form_back_to_list'] 		= 'Povratak na listu';
+	$lang['form_update_changes'] 	= 'Ažuriraj promjene';
+	$lang['form_cancel'] 			= 'Odustani';
+	$lang['form_update_loading'] 	= 'Učitavanje, ažuriranje promjena...';
+	$lang['update_success_message'] = 'Vaši podaci su uspješno ažurirani.';
+	$lang['form_go_back_to_list'] 	= 'Povratak na listu';
+
+	$lang['form_add'] 				= 'Dodaj';
+	$lang['insert_success_message'] = 'Vaši podaci su uspješno spremljeni u bazu.';
+	$lang['form_or']				= 'ili';
+	$lang['form_save'] 				= 'Spremi';
+	$lang['form_insert_loading'] 	= 'Učitavanje, spremanje podataka...';
+
+	$lang['form_upload_a_file'] 	= 'Upload datoteke';
+	$lang['form_upload_delete'] 	= 'briši';
+	$lang['form_button_clear'] 		= 'Poništi';
+
+	$lang['delete_success_message'] = 'Vaši podaci su uspješno izbrisani iz baze.';
+	$lang['delete_error_message'] 	= 'Vaši podaci nisusu izbrisani iz baze.';
+
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Uneseni podaci možda nisu spremljeni.\\nJeste li sigurni da se želite vratiti na listu?';
+	$lang['alert_edit_form']		= 'Promijenjeni podaci možda nisu spremljeni.\\nJeste li sigurni da se želite vratiti na listu?';
+	$lang['alert_delete']			= 'Jeste li sigurni da želite izbrisati ovaj zapis?';
+
+	$lang['insert_error']			= 'Dogodila se greška pri unošenju podataka.';
+	$lang['update_error']			= 'Dogodila se greška pri ažuriranju podataka.';
+
+	/* Added in version 1.2.1 */
+	$lang['set_relation_title']		= 'Odaberite {field_display_as}';
+	$lang['list_record']			= 'Zapis';
+	$lang['form_inactive']			= 'neaktivan';
+	$lang['form_active']			= 'activan';
+
+	/* Added in version 1.2.2 */
+	$lang['form_save_and_go_back']	= 'Spremi i vrati se na listu';
+	$lang['form_update_and_go_back']= 'Spremi i vrati se na listu';
+
+	/* Added in version 1.2.2 */
+	$lang['form_save_and_go_back']	= 'Spremi i vrati se na listu';
+	$lang['form_update_and_go_back']= 'Spremi i vrati se na listu';
+
+	/* Upload functionality */
+	$lang['string_delete_file'] 	= "Brisanje datoteke";
+	$lang['string_progress'] 		= "Napredak: ";
+	$lang['error_on_uploading'] 	= "Dogodila se greška prilikom upload-a.";
+	$lang['message_prompt_delete_file'] 	= "Jeste li sigurni da želite izbrisati ovu datoteku?";
+
+	$lang['error_max_number_of_files'] 	= "Odjednom možete upload-ati samo jednu datoteku.";
+	$lang['error_accept_file_types'] 	= "Nije dozvoljen upload ovog tipa datoteke.";
+	$lang['error_max_file_size'] 		= "Upload-ana datoteka je prekoračila {max_file_size} direktivu koja je specificirana.";
+	$lang['error_min_file_size'] 		= "Ne možete upload praznu datoteku.";
+
+	/* Added in version 1.3.1 */
+	$lang['list_export'] 	= "Izvoz";
+	$lang['list_print'] 	= "Ispis";
+	$lang['minimize_maximize'] = 'Smanji/Povečaj';
+	
+	/* Added in version 1.4 */
+	$lang['list_view'] = 'Pregled';
+
+	/* Added in version 1.5.1 */
+	$lang['ui_day'] = 'dd';
+	$lang['ui_month'] = 'mm';
+	$lang['ui_year'] = 'yyyy';
+
+	/* Added in version 1.5.2 */
+	$lang['list_more'] = 'Više';
+
