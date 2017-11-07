74dff982d519b073d6947e49580a9f2816f61b14
frely247 <fredrik.lysen@uadm.uu.se>
2017-11-07 11:00:22 +0100

    Swedish language file

---
+++ assets/grocery_crud/languages/swedish.php
@@ -0,0 +1,94 @@
+<?php
+	$lang['list_add'] 				= 'Addera';
+	$lang['list_actions'] 			= 'Kör';
+	$lang['list_page'] 				= 'Sida';
+	$lang['list_paging_of'] 		= 'av';
+	$lang['list_displaying']		= 'Visa {start} till {end} av {results} artiklar';
+	$lang['list_filtered_from']		= '(filtrerad från {total_results} av totalt poster)';
+	$lang['list_show_entries']		= 'Visa {paging} poster';
+	$lang['list_no_items']			= 'Inga artiklar att visa';
+	$lang['list_zero_entries']		= 'Visar 0 till 0 av 0 artiklar';
+	$lang['list_search'] 			= 'Sök';
+	$lang['list_search_all'] 		= 'Sök allt';
+	$lang['list_clear_filtering'] 	= 'Rensa filter';
+	$lang['list_delete'] 			= 'Ta bort';
+	$lang['list_edit'] 				= 'Ändra';
+	$lang['list_paging_first'] 		= 'Första';
+	$lang['list_paging_previous'] 	= 'Tidigare';
+	$lang['list_paging_next'] 		= 'Nästa';
+	$lang['list_paging_last'] 		= 'Sista';
+	$lang['list_loading'] 			= 'Laddar...';
+
+	$lang['form_edit'] 				= 'Editera';
+	$lang['form_back_to_list'] 		= 'Tillbaks till lista';
+	$lang['form_update_changes'] 	= 'Uppdatera ändringarna';
+	$lang['form_cancel'] 			= 'Avbryt';
+	$lang['form_update_loading'] 	= 'Laddar, uppdaterar ändringar...';
+	$lang['update_success_message'] = 'Din uppgifter har uppdateras.';
+	$lang['form_go_back_to_list'] 	= 'Gå tillbaka till listan';
+
+	$lang['form_add'] 				= 'Addera';
+	$lang['insert_success_message'] = 'Dina uppgifter har lagrats i databasen.';
+	$lang['form_or']				= 'eller';
+	$lang['form_save'] 				= 'Spara';
+	$lang['form_insert_loading'] 	= 'Laddar, sparar data...';
+
+	$lang['form_upload_a_file'] 	= 'Ladda upp en fil';
+	$lang['form_upload_delete'] 	= 'ta bort';
+	$lang['form_button_clear'] 		= 'Rensa';
+
+	$lang['delete_success_message'] = 'Dina uppgifter har tagits bort från databasen.';
+	$lang['delete_error_message'] 	= 'Dina uppgifter har inte tagits bort från databasen.';
+
+	/* Javascript messages */
+	$lang['alert_add_form']			= 'Data som du hade infoga kanske inte sparas.\\nÄr du säker på att du vill gåtillbaka till listan?';
+	$lang['alert_edit_form']		= 'Den data du hade ändrat kanske inte sparas.\\nÄr du säker på att du vill gåtillbaka till listan?';
+	$lang['alert_delete']			= 'Är du säker på att du vill radera den här posten?';
+
+	$lang['insert_error']			= 'Ett fel har inträffat vid infoga.';
+	$lang['update_error']			= 'Ett fel har inträffat vid spara.';
+
+	/* Added in version 1.2.1 */
+	$lang['set_relation_title']		= 'Välj {field_display_as}';
+	$lang['list_record']			= 'Post';
+	$lang['form_inactive']			= 'inaktiv';
+	$lang['form_active']			= 'aktiv';
+
+	/* Added in version 1.2.2 */
+	$lang['form_save_and_go_back']	= 'Spara och gå tillbaka till listan';
+	$lang['form_update_and_go_back']= 'Uppdatera och gå tillbaka till listan';
+
+	/* Upload functionality */
+	$lang['string_delete_file'] 	= "Ta bort fil";
+	$lang['string_progress'] 		= "Pågående: ";
+	$lang['error_on_uploading'] 	= "Ett fel har inträffat vid uppladdning.";
+	$lang['message_prompt_delete_file'] 	= "Är du säker på att du vill ta bort denna fil?";
+
+	$lang['error_max_number_of_files'] 	= "Du kan bara ladda upp en fil år gången.";
+	$lang['error_accept_file_types'] 	= "Du kan inte ladda upp en fil med denna ändelse.";
+	$lang['error_max_file_size'] 		= "Den uppladdade filen överskrider max {max_file_size} som uppgivits av administratören.";
+	$lang['error_min_file_size'] 		= "Du kan inte ladda upp en tom fil.";
+
+	/* Added in version 1.3.1 */
+	$lang['list_export'] 	= "Exportera";
+	$lang['list_print'] 	= "Printa";
+	$lang['minimize_maximize'] = 'Minimera/Maximera';
+	
+	/* Added in version 1.4 */
+	$lang['list_view'] = 'Vy';
+
+	/* Added in version 1.5.1 */
+	$lang['ui_day'] = 'dd';
+	$lang['ui_month'] = 'mm';
+	$lang['ui_year'] = 'yyyy';
+
+	/* Added in version 1.5.2 */
+	$lang['list_more'] = 'Mer';
+
+	/* Added in version 1.5.6 */
+	$lang['list_search_column'] = 'Sök {column_name}';
+
+	/* Added in version 1.5.8 */
+	$lang['alert_delete_multiple'] = 'Är du säker på att du vill ta bort dessa {items_amount} poster?';
+	$lang['alert_delete_multiple_one'] = 'Är du säker på att du vill ta bort denna post?';
+
